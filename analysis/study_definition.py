9e738dfb543e1c6cd4b158f1efb7764d0651dee0
Venexia Walker <venexia.walker@bristol.ac.uk>
2021-10-25 12:53:50 +0100

    Initial commit

---
+++ analysis/study_definition.py
@@ -0,0 +1,13 @@
+from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
+
+
+study = StudyDefinition(
+    default_expectations={
+        "date": {"earliest": "1900-01-01", "latest": "today"},
+        "rate": "uniform",
+        "incidence": 0.5,
+    },
+    population=patients.registered_with_one_practice_between(
+        "2019-02-01", "2020-02-01"
+    ),
+)
