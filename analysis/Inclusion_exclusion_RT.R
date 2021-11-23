315c980a6438e2848ab42f6bfefac8ad24b1cde1
tomsrenin <renin.toms@bristol.ac.uk>
2021-11-23 11:42:42 +0000

    Update Inclusion_exclusion_RT.R
    Inclusion criteria 1 added

--- analysis/Inclusion_exclusion_RT.R
+++ analysis/Inclusion_exclusion_RT.R
@@ -7,8 +7,8 @@
 #Ref: https://www.health.gov.au/initiatives-and-programs/covid-19-vaccines/is-it-true/is-it-true-how-long-does-it-take-to-have-immunity-after-vaccination
 
 #PRESET change dates to date format
-data1$covid19_vaccination_date1 <- as.Date(data1$covid19_vaccination_date1,tryFormats = c("%Y-%m-%d") )
-data1$covid19_vaccination_date2 <- as.Date(data1$covid19_vaccination_date2,tryFormats = c("%Y-%m-%d") )
+data1$covid19_vaccination_date1 <- as.Date(data1$covid19_vaccination_date1,tryFormats = c("%Y-%m-%d"))
+data1$covid19_vaccination_date2 <- as.Date(data1$covid19_vaccination_date2,tryFormats = c("%Y-%m-%d"))
 data1$death_covid19_date <- as.Date(data1$death_covid19_date,tryFormats = c("%Y-%m-%d"))
 data1$primary_care_death_date <- as.Date(data1$primary_care_death_date,tryFormats = c("%Y-%m-%d"))
 data1$ons_died_from_any_cause_date <- as.Date(data1$ons_died_from_any_cause_date,tryFormats = c("%Y-%m-%d"))
@@ -20,3 +20,17 @@ data1$delta_start <- as.Date("2021-06-01")
 data1$immune_start <- as.Date(data1$covid19_vaccination_date2)+15
 #c.latest of a,b as COHORT start date
 data1$vacc_coh_start_date <- pmax(data1$delta_start, data1$immune_start, na.rm = TRUE)
+
+#INCLUSION CRITERIA 1.Alive on the first day of follow up
+#a.Determine the death date
+data1$vacc_coh_death_date <- pmin(data1$death_covid19_date, data1$primary_care_death_date, data1$ons_died_from_any_cause_date, na.rm = TRUE)
+#Adopted the earliest available death date
+
+#b.determine the living status on start date
+data1$start_alive <- ifelse(data1$vacc_coh_death_date < data1$vacc_coh_start_date, 0, 1)# 1- alive; 0 - died
+data1$start_alive[is.na(data1$start_alive)] <- 1
+table(data1$start_alive, useNA = "ifany")# ~482 died before the start date
+
+#subset data based on alive status on day 1 of follow up.
+data2 <- subset(data1, data1$start_alive > 0) #~ 9518 samples retained
+
