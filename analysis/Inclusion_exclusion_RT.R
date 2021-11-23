4c7b2ec10ffaad076fbaac4e9c166d780940b9c4
tomsrenin <renin.toms@bristol.ac.uk>
2021-11-23 10:28:27 +0000

    Create Inclusion_exclusion_RT.R
    1st commit - study start date

---
+++ analysis/Inclusion_exclusion_RT.R
@@ -0,0 +1,22 @@
+#Project:Vaccinated delta wave population study
+#Branch:Inclusion exclusion criteria 
+#Scripts: Renin Toms
+
+#1. Set the follow up date - 2 weeks(14 days) after the 2nd dose of vaccination
+#Reason "Individuals may not have the best protection until 7–14 days after their second dose of the vaccine"
+#Ref: https://www.health.gov.au/initiatives-and-programs/covid-19-vaccines/is-it-true/is-it-true-how-long-does-it-take-to-have-immunity-after-vaccination
+
+#PRESET change dates to date format
+data1$covid19_vaccination_date1 <- as.Date(data1$covid19_vaccination_date1,tryFormats = c("%Y-%m-%d") )
+data1$covid19_vaccination_date2 <- as.Date(data1$covid19_vaccination_date2,tryFormats = c("%Y-%m-%d") )
+data1$death_covid19_date <- as.Date(data1$death_covid19_date,tryFormats = c("%Y-%m-%d"))
+data1$primary_care_death_date <- as.Date(data1$primary_care_death_date,tryFormats = c("%Y-%m-%d"))
+data1$ons_died_from_any_cause_date <- as.Date(data1$ons_died_from_any_cause_date,tryFormats = c("%Y-%m-%d"))
+
+#COHORT START DATE
+#a.start date 2021-06-1 of the cohort if not vaccinated
+data1$delta_start <- as.Date("2021-06-01")
+#b.15 days after the second vaccination
+data1$immune_start <- as.Date(data1$covid19_vaccination_date2)+15
+#c.latest of a,b as COHORT start date
+data1$vacc_coh_start_date <- pmax(data1$delta_start, data1$immune_start, na.rm = TRUE)
