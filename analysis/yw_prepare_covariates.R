c5d3e0364407a65385cbcedaed8c6f2f0cceb83e
Yinghui Wei <91749253+yinghuiwei@users.noreply.github.com>
2021-11-24 21:30:58 +0000

    combine groups in deprivation to make 5 groups: most deprived (1-2); second (3-4); third(5-6); fourth (7-8); least deprived (9-10).
    0 - Missing

--- analysis/yw_prepare_covariates.R
+++ analysis/yw_prepare_covariates.R
@@ -12,7 +12,7 @@
 ##           2. Check that continuous variables are defined as numeric variables.
 ##           3. check the number and percentage of missing values for covariates
 ## =============================================================================
-
+#
 # Notes:
 # Assumption applied: age, deprivation and number of disorders are currently assumed to be continuous variables
 #
@@ -20,10 +20,14 @@
 #    1. table(data$cov_n_disorder) includes a negative value -1
 #    2. table(data$cov_ethnicity) returns 17 ethnic groups, should merge some.
 #       no names for ethnicity, all in numbers
-#    3. cov_smoking_status = E, M, N, S, needs more detailed description in the text about these labels
+#    3. cov_smoking_status = E, M, N, S, needs more detailed descriptions in the text about these labels
 
 # YW Notes:
-#    1. No missing values in covariates apart from smoking status, 50% missing in this covariates
+#    1. No missing values in covariates apart from smoking status, 50% missing in this covariate
+
+# YW 24/Nov/2021 make deprivation as a categorical variable
+#                deprivation: 11 unique integers, from 0 to 10. 
+#                group 1-2 (most deprived) 3-4, 5-6, 7-8, 9-10 (least deprived), is 0 missing?
 
 library(readr); library(dplyr); library(stringr)
 
@@ -60,7 +64,7 @@ covars$cov_region <- gsub(" ", "_", covars$cov_region)
 print(unique(covars$cov_region))
 
 # names of variables which are factors: only two variables are continuous, all others or binary or categorical
-factor_names <- names(covars %>% dplyr::select(! c("cov_age", "cov_deprivation", "cov_n_disorder")))
+factor_names <- names(covars %>% dplyr::select(! c("cov_age", "cov_n_disorder")))
 
 # The variables that should be factor variables
 covars[,factor_names] <- lapply(covars[,factor_names] , factor)
@@ -83,7 +87,7 @@ for (colname in factor_names){
 }
 
 ##------Relevel to set the group which has the highest frequency as reference group -----------------------------------------
-# check the frequencies for each factor level
+# check the frequency for each factor level
 lapply(data[,factor_names], table)
 
 # Find mode in a factor variable
@@ -98,13 +102,20 @@ covars$cov_ethnicity = relevel(covars$cov_ethnicity, ref = as.character(calculat
 covars$cov_smoking_status = relevel(covars$cov_smoking_status, ref = as.character(calculate_mode(covars$cov_smoking_status)))
 covars$cov_region = relevel(covars$cov_region, ref = as.character(calculate_mode(covars$cov_region)))
 
+#combine groups in deprivation: First - most deprived; fifth -least deprived
+levels(covars$cov_deprivation)[levels(covars$cov_deprivation)==1 | levels(covars$cov_deprivation)==2] <-"1-2 (most deprived)"
+levels(covars$cov_deprivation)[levels(covars$cov_deprivation)==3 | levels(covars$cov_deprivation)==4] <-"3-4"
+levels(covars$cov_deprivation)[levels(covars$cov_deprivation)==5 | levels(covars$cov_deprivation)==6] <-"5-6"
+levels(covars$cov_deprivation)[levels(covars$cov_deprivation)==7 | levels(covars$cov_deprivation)==8] <-"7-8"
+levels(covars$cov_deprivation)[levels(covars$cov_deprivation)==9 | levels(covars$cov_deprivation)==10] <-"9-10 (least deprived)"
+
 # a simple check if factor reference level has changed
 lapply(covars[,c("cov_ethnicity", "cov_smoking_status", "cov_region")], table)
-lapply(covars[,names], table)
+lapply(covars[,factor_names], table)
 
 ##------------------------------- NUMERICAL Variables --------------------------------------
 # Checking if continuous covariates are set up as numeric variable correctly
-is.numeric(data$cov_n_disorder); is.numeric(data$cov_age); is.numeric(data$cov_deprivation)
+is.numeric(data$cov_n_disorder); is.numeric(data$cov_age); 
 str(covars)
 
 data[,covariate_names] <- covars
