ecce24c19e1b8383693fddf87902bc0e92fbd68f
RochelleKnight <rochelle.knight@bristol.ac.uk>
2021-11-25 12:19:02 +0000

    Update QA script

--- analysis/Quality_Assurance_script.R
+++ analysis/Quality_Assurance_script.R
@@ -1,4 +1,5 @@
-#Quality Assurance - *NOT FINISHED*
+#Quality Assurance
+#Has various rules that must be met to be included in the study popultaion - checks that the data follows general common sense rules
 #Need to decide on the best way to read in data/sharing data between scripts.
 
 library(dplyr)
@@ -7,10 +8,11 @@ input <- fread("OpenSafely - unvaccinated analysis/input.csv" ,na.strings=c("","
 
 
 #I'm assuming this will all be done in the preparing covariates script instead
-outcome=colnames(input)[grepl("out_",colnames(input))]
-qa_variables=colnames(input)[grepl("qa_",colnames(input))]
-contain_date=colnames(input)[grepl("date",colnames(input))]
-date_columns=c(outcome,qa_variables,contain_date)
+#outcome=colnames(input)[grepl("out_",colnames(input))]
+#qa_variables=colnames(input)[grepl("qa_",colnames(input))]
+#contain_date=colnames(input)[grepl("date",colnames(input))]
+#date_columns=c(outcome,qa_variables,contain_date)
+date_columns=colnames(input)[grepl("\\d\\d\\d\\d-\\d\\d-\\d\\d",input)]
 
 
 for(date in date_columns){
@@ -29,7 +31,6 @@ input$rule2=NA
 input$rule2=(is.na(input$patient_id)|is.na(input$cov_age)|is.na(input$cov_sex)|is.na(input$cov_deprivation)|is.na(input$cov_ethnicity)|is.na(input$cov_region)|is.na(input$cov_n_disorder)|is.na(input$cov_smoking_status)|is.na(input$birth_year))
 #input$rule2=(is.na(input$patient_id)|is.na(input$cov_age)|is.na(input$cov_sex)|is.na(input$cov_deprivation)|is.null(input$patient_id)|is.null(input$cov_age)|is.null(input$cov_sex)|is.null(input$cov_deprivation)|is.nan(input$patient_id)|is.nan(input$cov_age)|is.nan(input$cov_sex)|is.nan(input$cov_deprivation))
 #I think is.na would probably be enough but not sure
-
 
 
 #Rule 3: Year of birth predates NHS established year or year of birth exceeds current date
@@ -66,5 +67,33 @@ input$rule9=((input$cov_sex=="M" & input$cov_hormone_replacement_therapy==1)|(in
 
 
 #Remove rows that are TRUE for at least one rule
-input_QA=input[rule1==F & rule2==F & rule3==F & rule4==F & rule6==F & rule7==F & rule9==F,]
+input_QA=input[rule1==F & rule2==F & rule3==F & rule4==F & rule6 & rule7==F & rule9==F,]
 input_QA=input_QA %>% select(-c(rule1,rule2,rule3,rule4,rule6,rule7,rule9))
+
+
+#QA summary
+QA_summary <- data.frame(matrix(ncol = 2))
+colnames(QA_summary) <- c('Rule', '# where rule true')
+QA_summary[1,1]="Rule 1"
+QA_summary[1,2]=nrow(input%>%filter(rule1==T))
+QA_summary[2,1]="Rule 2"
+QA_summary[2,2]=nrow(input%>%filter(rule2==T))
+QA_summary[3,1]="Rule 3"
+QA_summary[3,2]=nrow(input%>%filter(rule3==T))
+QA_summary[4,1]="Rule 4"
+QA_summary[4,2]=nrow(input%>%filter(rule4==T))
+QA_summary[5,1]="Rule 6"
+QA_summary[5,2]=nrow(input%>%filter(rule6==T))
+QA_summary[6,1]="Rule 7"
+QA_summary[6,2]=nrow(input%>%filter(rule7==T))
+QA_summary[7,1]="Rule 9"
+QA_summary[7,2]=nrow(input%>%filter(rule9==T))
+QA_summary[8,1]="Total excluded from QA"
+QA_summary[8,2]=nrow(input)-nrow(input_QA)
+
+#write.csv(QA_summary, file = file.path("output", paste0("QA_summary", ".csv")) , row.names=F)
+
+
+
+
+
