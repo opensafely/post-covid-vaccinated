d3440e393f7a8df638a95c2de08c86d9c90111c2
Genevieve Cezard <gic30@medschl.cam.ac.uk>
2021-12-13 13:27:59 +0000

    Update 1a-1b-1c to include QA, vax and exp variables
    The initial code was created to deal with covariates only.
    The code is now updated to include all QA, vaccination and exposure variables that are either binary, categorical or numeric.

--- analysis/Stage1_data_cleaning.R
+++ analysis/Stage1_data_cleaning.R
@@ -46,11 +46,11 @@ input <-read_rds("output/input.rds") #View(input)
 ######################################################
 # 1. Prepare all variables (re-factoring, re-typing) # 
 ######################################################
-# extract names of covariates
-covariate_names <- tidyselect::vars_select(names(input), starts_with('cov_', ignore.case = TRUE))
 
-# create a data frame for covariates
-covars <- input[,covariate_names]
+# Extract names of variables
+variable_names <- tidyselect::vars_select(names(input), starts_with('cov_','qa_','vax_cat','exp_cat', ignore.case = TRUE))
+# Create a data frame for all relevant variables
+covars <- input[,variable_names] #View(covars)
 
 # Replace " " with "_"
 covars$cov_cat_region <- gsub(" ", "_", covars$cov_cat_region)
@@ -58,19 +58,21 @@ covars$cov_cat_region <- gsub(" ", "_", covars$cov_cat_region)
 #-------------------------------------#
 # 1.a. Set factor variables as factor #
 #-------------------------------------#
-# names of variables which are factors
+# Get the names of variables which are factors
 factor_names_bin <- tidyselect::vars_select(names(input), starts_with('cov_bin', ignore.case = TRUE))
 factor_names_cat <- tidyselect::vars_select(names(input), starts_with('cov_cat', ignore.case = TRUE))
+factor_names_qa_bin <- tidyselect::vars_select(names(input), starts_with('qa_bin', ignore.case = TRUE))
+factor_names_vax_cat <- tidyselect::vars_select(names(input), starts_with('vax_cat', ignore.case = TRUE))
+factor_names_exp_cat <- tidyselect::vars_select(names(input), starts_with('exp_cat', ignore.case = TRUE))
+factor_names <- c(factor_names_bin, factor_names_cat, factor_names_qa_bin, factor_names_vax_cat, factor_names_exp_cat)
 
-factor_names <- c(factor_names_bin, factor_names_cat)
-
-# The variables that should be factor variables
+# Set the variables that should be factor variables as factor
 covars[,factor_names] <- lapply(covars[,factor_names] , factor)
 
-# check the property of variables
+# Check the property of variables
 #str(covars)
 
-# sort factor level alphabetically
+# Sort factor level alphabetically
 mk_factor_orderlevels <- function(covars, colname)
 {
   covars <- covars %>% mutate(
@@ -89,7 +91,7 @@ for (colname in factor_names){
 #----------------------------------------------------------------------#
 # Relevel
 
-# check the frequency for each factor level
+# Check the frequency for each factor level
 #lapply(input[,factor_names], table)
 
 # Find mode in a factor variable
@@ -99,10 +101,17 @@ calculate_mode <- function(x) {
 }
 
 # For the following variables, the first level (reference level) is not the one with the highest frequency
-# set the most frequently occurred level as the reference reference for a factor variable
+# Set the most frequently occurred level as the reference for a factor variable
 covars$cov_cat_ethnicity = relevel(covars$cov_cat_ethnicity, ref = as.character(calculate_mode(covars$cov_cat_ethnicity)))
 covars$cov_cat_smoking_status = relevel(covars$cov_cat_smoking_status, ref = as.character(calculate_mode(covars$cov_cat_smoking_status)))
 covars$cov_cat_region = relevel(covars$cov_cat_region, ref = as.character(calculate_mode(covars$cov_cat_region)))
+
+covars$exp_cat_covid19_hospital = relevel(covars$exp_cat_covid19_hospital, ref = as.character(calculate_mode(covars$exp_cat_covid19_hospital)))
+
+covars$vax_cat_jcvi_group = relevel(covars$vax_cat_jcvi_group, ref = as.character(calculate_mode(covars$vax_cat_jcvi_group)))
+covars$vax_cat_product_1 = relevel(covars$vax_cat_product_1, ref = as.character(calculate_mode(covars$vax_cat_product_1)))
+covars$vax_cat_product_2 = relevel(covars$vax_cat_product_2, ref = as.character(calculate_mode(covars$vax_cat_product_2)))
+covars$vax_cat_product_3 = relevel(covars$vax_cat_product_3, ref = as.character(calculate_mode(covars$vax_cat_product_3)))
 
 #combine groups in deprivation: First - most deprived; fifth -least deprived
 levels(covars$cov_cat_deprivation)[levels(covars$cov_cat_deprivation)==1 | levels(covars$cov_cat_deprivation)==2] <-"1-2 (most deprived)"
@@ -110,8 +119,9 @@ levels(covars$cov_cat_deprivation)[levels(covars$cov_cat_deprivation)==3 | level
 levels(covars$cov_cat_deprivation)[levels(covars$cov_cat_deprivation)==5 | levels(covars$cov_cat_deprivation)==6] <-"5-6"
 levels(covars$cov_cat_deprivation)[levels(covars$cov_cat_deprivation)==7 | levels(covars$cov_cat_deprivation)==8] <-"7-8"
 levels(covars$cov_cat_deprivation)[levels(covars$cov_cat_deprivation)==9 | levels(covars$cov_cat_deprivation)==10] <-"9-10 (least deprived)"
+covars$cov_cat_deprivation = relevel(covars$cov_cat_deprivation, ref = as.character(calculate_mode(covars$cov_cat_deprivation))) # added
 
-# a simple check if factor reference level has changed
+# A simple check if factor reference level has changed
 #lapply(covars[,c("cov_ethnicity", "cov_smoking_status", "cov_region")], table)
 meta_data_factors <- lapply(covars[,factor_names], table)
 
@@ -125,5 +135,8 @@ sink()
 #----------------------------------------------------------------------#
 # Notes: Age, number of GP consultations and year of birth are continuous variables
 
-#is.numeric(data$cov_num_age); is.numeric(data$cov_num_consulation_rate); 
+# Checking if continuous variables are set up as numeric variable correctly
+#is.numeric(data$cov_num_age)
+#is.numeric(data$cov_num_consulation_rate)
+#is.numeric(data$qa_num_birth_year)
 #str(covars)
