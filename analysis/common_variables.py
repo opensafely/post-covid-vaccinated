7c67e8a75c06e95be0dcd227de8765cb7e1c0cfa
yinghuiwei <yinghui.wei@plymouth.ac.uk>
2021-12-16 19:33:53 +0000

    add variables

--- analysis/common_variables.py
+++ analysis/common_variables.py
@@ -1056,8 +1056,29 @@ def generate_common_variables(index_date_variable):
     ),
 
     # "cov_bin_combined_oral_contraceptive_pill": [cocp_dmd],
+    ### dmd: dictionary of medicines and devices
+    tmp_cov_bin_combined_oral_contraceptive_pill_dmd=patients.with_these_clinical_events(
+        cocp_dmd, 
+        returning='binary_flag',
+        on_or_before=f"{index_date_variable}",
+        return_expectations={"incidence": 0.01},
+    ),
+    ### Combined
+    cov_bin_combined_oral_contraceptive_pill= patients.maximum_of(
+        "tmp_cov_bin_combined_oral_contraceptive_pill_dmd",
+    ),
 
     # "cov_bin_hormone_replacement_therapy": [hrt_dmd],
+    tmp_cov_bin_hormone_replacement_therapy_dmd=patients.with_these_clinical_events(
+        hrt_dmd, 
+        returning='binary_flag',
+        on_or_before=f"{index_date_variable}",
+        return_expectations={"incidence": 0.01},
+    ),
+    ### Combined
+    cov_bin_hormone_replacement_therapy= patients.maximum_of(
+        "tmp_cov_bin_hormone_replacement_therapy_dmd",
+    ),
     # "sub_bin_ate": [ami_snomed_clinical, ami_icd10, ami_prior_icd10, other_arterial_embolism_icd10, stroke_isch_icd10, stroke_isch_snomed_clinical],  
 
     )
