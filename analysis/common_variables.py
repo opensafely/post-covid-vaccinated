6fd1082b2ab0efc882e14af712c80143fc8a6e05
Yinghui Wei <91749253+yinghuiwei@users.noreply.github.com>
2021-12-16 22:21:42 +0000

    add variables

--- analysis/common_variables.py
+++ analysis/common_variables.py
@@ -1079,8 +1079,53 @@ def generate_common_variables(index_date_variable):
     cov_bin_hormone_replacement_therapy= patients.maximum_of(
         "tmp_cov_bin_hormone_replacement_therapy_dmd",
     ),
-    # "sub_bin_ate": [ami_snomed_clinical, ami_icd10, ami_prior_icd10, other_arterial_embolism_icd10, stroke_isch_icd10, stroke_isch_snomed_clinical],  
 
+    # "sub_bin_ate": [ami_snomed_clinical, ami_icd10, ami_prior_icd10, other_arterial_embolism_icd10, stroke_isch_icd10, stroke_isch_snomed_clinical],  
+    ### Primar care
+    tmp_sub_bin_ate_snomed=patients.with_these_clinical_events(
+        ami_snomed_clinical,
+        returning='binary_flag',
+        on_or_before=f"{index_date_variable}",
+        return_expectations={"incidence": 0.01},
+    ),
+    ### HES
+    tmp_sub_bin_ate_hes=patients.admitted_to_hospital(
+        returning='binary_flag',
+        with_these_diagnoses= ami_icd10,
+        on_or_before=f"{index_date_variable}",
+        return_expectations={"incidence": 0.01},
+    ),
+    ### Prior HES
+    tmp_sub_bin_ate_prior_hes=patients.admitted_to_hospital(
+        returning='binary_flag',
+        with_these_diagnoses= ami_prior_icd10,
+        on_or_before=f"{index_date_variable}",
+        return_expectations={"incidence": 0.01},
+    ),
+    ### HES
+    tmp_sub_bin_other_arterial_embolism_hes=patients.admitted_to_hospital(
+        returning='binary_flag',
+        with_these_diagnoses= other_arterial_embolism_icd10,
+        on_or_before=f"{index_date_variable}",
+        return_expectations={"incidence": 0.01},
+    ),
+    ### HES
+    tmp_sub_bin_stroke_isch_hes=patients.admitted_to_hospital(
+        returning='binary_flag',
+        with_these_diagnoses= stroke_isch_icd10,
+        on_or_before=f"{index_date_variable}",
+        return_expectations={"incidence": 0.01},
+    ),
+    tmp_sub_bin_stroke_isch_snomed=patients.with_these_clinical_events(
+        stroke_isch_snomed_clinical,
+        returning='binary_flag',
+        on_or_before=f"{index_date_variable}",
+        return_expectations={"incidence": 0.01},
+    ),
+    ### Combined
+    sub_bin_ate= patients.maximum_of(
+        "tmp_sub_bin_ate_snomed", "tmp_sub_bin_ate_hes", "tmp_sub_bin_ate_prior_hes", "tmp_sub_bin_other_arterial_embolism_hes","tmp_sub_bin_stroke_isch_hes","tmp_sub_bin_stroke_isch_snomed",
+    ),
     )
 
     return dynamic_variables
