00ef4150c9d205b20370ea8cf3f157791c12f439
Yinghui Wei <91749253+yinghuiwei@users.noreply.github.com>
2021-12-20 15:32:36 +0000

    simplify code for variables with only one data source (dmd)

--- analysis/common_variables.py
+++ analysis/common_variables.py
@@ -816,7 +816,7 @@ def generate_common_variables(index_date_variable):
         on_or_before=f"{index_date_variable}",
         return_expectations={"incidence": 0.01},
     ),
-    ### HES APC
+    ### HES APC (Hospital Episode Statistics Admitted Patient)
     tmp_cov_bin_dementia_hes=patients.admitted_to_hospital(
         returning='binary_flag',
         with_these_diagnoses=dementia_icd10,
@@ -1018,67 +1018,67 @@ def generate_common_variables(index_date_variable):
 
     # "cov_bin_lipid_medications": [lipid_lowering_dmd],
     ### dmd: dictionary of medicines and devices
-    tmp_cov_bin_lipid_medications_dmd=patients.with_these_clinical_events(
+    cov_bin_lipid_medications_dmd=patients.with_these_clinical_events(
         lipid_lowering_dmd,
         returning='binary_flag',
         on_or_before=f"{index_date_variable}",
         return_expectations={"incidence": 0.01},
     ),
     ### Combined
-    cov_bin_lipid_medications_dmd=patients.maximum_of(
-        "tmp_cov_bin_lipid_medications_dmd", 
-    ),
+    # cov_bin_lipid_medications_dmd=patients.maximum_of(
+    #     "tmp_cov_bin_lipid_medications_dmd", 
+    # ),
 
     # "cov_bin_antiplatelet_medications": [antiplatelet_dmd],
     ### dmd: dictionary of medicines and devices
-    tmp_cov_bin_antiplatelet_medications=patients.with_these_clinical_events(
+    cov_bin_antiplatelet_medications=patients.with_these_clinical_events(
         antiplatelet_dmd,
         returning='binary_flag',
         on_or_before=f"{index_date_variable}",
         return_expectations={"incidence": 0.01},
     ),
     ### Combined
-    cov_bin_antiplatelet_medications=patients.maximum_of(
-        "tmp_cov_bin_antiplatelet_medications", 
-    ),
+    # cov_bin_antiplatelet_medications=patients.maximum_of(
+    #     "tmp_cov_bin_antiplatelet_medications", 
+    # ),
 
     # "cov_bin_anticoagulation_medications": [anticoagulant_dmd],
     ### dmd: dictionary of medicines and devices
-    tmp_cov_bin_anticoagulation_medications=patients.with_these_clinical_events(
+    cov_bin_anticoagulation_medications=patients.with_these_clinical_events(
         anticoagulant_dmd, 
         returning='binary_flag',
         on_or_before=f"{index_date_variable}",
         return_expectations={"incidence": 0.01},
     ),
-    ### Combined
-    cov_bin_anticoagulation_medications= patients.maximum_of(
-        "tmp_cov_bin_anticoagulation_medications",
-    ),
+    # ### Combined
+    # cov_bin_anticoagulation_medications= patients.maximum_of(
+    #     "tmp_cov_bin_anticoagulation_medications",
+    # ),
 
     # "cov_bin_combined_oral_contraceptive_pill": [cocp_dmd],
     ### dmd: dictionary of medicines and devices
-    tmp_cov_bin_combined_oral_contraceptive_pill_dmd=patients.with_these_clinical_events(
+    cov_bin_combined_oral_contraceptive_pill_dmd=patients.with_these_clinical_events(
         cocp_dmd, 
         returning='binary_flag',
         on_or_before=f"{index_date_variable}",
         return_expectations={"incidence": 0.01},
     ),
-    ### Combined
-    cov_bin_combined_oral_contraceptive_pill= patients.maximum_of(
-        "tmp_cov_bin_combined_oral_contraceptive_pill_dmd",
-    ),
+    # ### Combined
+    # cov_bin_combined_oral_contraceptive_pill= patients.maximum_of(
+    #     "tmp_cov_bin_combined_oral_contraceptive_pill_dmd",
+    # ),
 
     # "cov_bin_hormone_replacement_therapy": [hrt_dmd],
-    tmp_cov_bin_hormone_replacement_therapy_dmd=patients.with_these_clinical_events(
+    cov_bin_hormone_replacement_therapy_dmd=patients.with_these_clinical_events(
         hrt_dmd, 
         returning='binary_flag',
         on_or_before=f"{index_date_variable}",
         return_expectations={"incidence": 0.01},
     ),
-    ### Combined
-    cov_bin_hormone_replacement_therapy= patients.maximum_of(
-        "tmp_cov_bin_hormone_replacement_therapy_dmd",
-    ),
+    # ### Combined
+    # cov_bin_hormone_replacement_therapy= patients.maximum_of(
+    #     "tmp_cov_bin_hormone_replacement_therapy_dmd",
+    # ),
     # "sub_bin_ate": [ami_snomed_clinical, ami_icd10, ami_prior_icd10, other_arterial_embolism_icd10, stroke_isch_icd10, stroke_isch_snomed_clinical],  
     ### Primary care
     tmp_sub_bin_ate_snomed=patients.with_these_clinical_events(
