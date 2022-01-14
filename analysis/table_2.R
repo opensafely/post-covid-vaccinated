d67178b03f9f4786e73cefd685150a176dc0a60e
Yinghui Wei <91749253+yinghuiwei@users.noreply.github.com>
2022-01-14 12:49:02 +0000

    outcome events

--- analysis/table_2.R
+++ analysis/table_2.R
@@ -30,5 +30,53 @@ if(population == "electively_unvaccinated"){
 
 n_events <- rep(0,10)
 
-# outcome 1: ami
-n_events[1] <- length(which(!is.na(input$out_date_ami)))
+number_events <- function(outcome)
+{
+  count <- length(which(!is.na(outcome)))
+  return(count)
+}
+
+outcome_names <- c("out_date_ami", "out_date_stroke_isch", 
+                   "out_date_pe", "out_date_dvt",
+                    "out_date_tia", "out_date_stroke_sah_hs", 
+                   "out_date_hf", "out_date_angina",
+                   "out_date_ate", "out_date_vte"
+                   )
+
+n_events <- c(lapply(input[,outcome_names], number_events))
+
+n_events
+
+# # outcome 1: ami
+# n_events[1] <- number_events(input$out_date_ami)
+# 
+# # outcome 2: stroke
+# n_events[2] <- number_events(input$out_date_stroke_isch)
+# 
+# # outcome 3: pe
+# n_events[3] <- number_events(input$out_date_pe)
+# 
+# # outcome 4: dvt
+# n_events[4] <- number_events(input$out_date_dvt)
+# 
+# # outcome 5: tia
+# n_events[5] <- number_events(input$out_date_tia)
+# 
+# # outcome 6: stroke_sah_hs
+# n_events[6] <- number_events(input$out_date_stroke_sah_hs)
+# 
+# # outcome 7: hf
+# n_events[7] <- number_events(input$out_date_hf)
+# 
+# # outcome 8: angina
+# n_events[8] <- number_events(input$out_date_angina)
+# 
+# # outcome 9: ate (Other Arterial Embolism)
+# n_events[9] <- number_events(input$out_date_ate)
+# 
+# # outcome 10: vte
+# n_events[10] <- number_events(input$out_date_vte)
+# 
+# 
+
+
