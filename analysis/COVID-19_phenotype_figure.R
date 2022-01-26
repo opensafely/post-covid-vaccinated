4add48945ba78a79efa66a6592773fb2e0b18c9e
RochelleKnight <rochelle.knight@bristol.ac.uk>
2022-01-26 12:54:48 +0000

    Update COVID-19_phenotype_figure.R

--- analysis/COVID-19_phenotype_figure.R
+++ analysis/COVID-19_phenotype_figure.R
@@ -24,18 +24,18 @@ pheno_results <- pheno_results %>% filter(str_detect(term, "^days"))
 # Where to plot the time period plots along the x-axis---------------------------
 
 #In days since COVID
-term_to_time <- data.frame(term = c("days0_14","days14_28","days28_56","days56_84","days84_196",
-                                   "days0_28","days28_196"),
-                          time = c(7,21,42,70,140,
-                                   14,112))
+# term_to_time <- data.frame(term = c("days0_14","days14_28","days28_56","days56_84","days84_196",
+#                                    "days0_28","days28_196"),
+#                           time = c(7,21,42,70,140,
+#                                    14,112))
 
 #In weeks since COVID
 #weeks0_2, weeks2_4, weeks4_8, weeks8_12, weeks12_28
 #weeks0_4, weeks4_28
-# term_to_time <- data.frame(term = c("days0_14","days14_28","days28_56","days56_84","days84_196",
-#                                    "days0_28","days28_196"),
-#                           time = c(1,3,6,10,20,
-#                                    2,16))
+term_to_time <- data.frame(term = c("days0_14","days14_28","days28_56","days56_84","days84_196",
+                                   "days0_28","days28_196"),
+                          time = c(1,3,6,10,20,
+                                   2,16))
 
 pheno_results <- merge(pheno_results, term_to_time, by = c("term"), all.x = TRUE)
 
@@ -86,24 +86,28 @@ pheno_results$event <- factor(pheno_results$event, levels=c("Acute myocardial in
 
 # Plot figures
 
+min_plot=0.25
+max_plot=64
+
 ggplot2::ggplot(data =pheno_results,
                 mapping = ggplot2::aes(x = time, y = estimate, color = strata, shape=strata, fill=strata)) +
   ggplot2::facet_wrap(event~., ncol = 2)+
   ggplot2::geom_hline(mapping = ggplot2::aes(yintercept = 1), colour = "#A9A9A9") +
   ggplot2::geom_point(position = ggplot2::position_dodge(width = 1)) +
-  ggplot2::geom_errorbar(mapping = ggplot2::aes(ymin = ifelse(conf.low<0.25,0.25,conf.low), 
-                                                ymax = ifelse(conf.high>64,64,conf.high),  
+  ggplot2::geom_errorbar(mapping = ggplot2::aes(ymin = ifelse(conf.low<min_plot,min_plot,conf.low), 
+                                                ymax = ifelse(conf.high>max_plot,max_plot,conf.high),  
                                                 width = 0), 
                          position = ggplot2::position_dodge(width = 1)) +
   ggplot2::geom_line(position = ggplot2::position_dodge(width = 1)) +
+  #ggplot2::scale_y_continuous(lim = c(0,3)) +
   #ggplot2::scale_y_continuous(lim = c(0.25,64), breaks = c(0.5,0.5,1,2,4,8,16,32,64), trans = "log") +
-  ggplot2::scale_x_continuous(lim = c(0,196), breaks = seq(0,196,28)) +
-  #ggplot2::scale_x_continuous(lim = c(0,28), breaks = seq(0,28,4)) +
-  ggplot2::scale_fill_manual(values = levels(pheno_results$colour)[1:2], labels = levels(pheno_results$strata)[1:2]) +
-  ggplot2::scale_color_manual(values = levels(pheno_results$colour)[1:2], labels = levels(pheno_results$strata)[1:2]) +
-  ggplot2::scale_shape_manual(values = c(rep(c(21,22),3),23,24)[1:2], labels = levels(pheno_results$strata)[1:1]) +
-  ggplot2::labs(x = "\nDays since COVID-19 diagnosis", y = "Hazard ratio and 95% confidence interval") +
-  #ggplot2::labs(x = "\nWeeks since COVID-19 diagnosis", y = "Hazard ratio and 95% confidence interval") +
+  #ggplot2::scale_x_continuous(lim = c(0,196), breaks = seq(0,196,28)) +
+  ggplot2::scale_x_continuous(lim = c(0,28), breaks = seq(0,28,4)) +
+  ggplot2::scale_fill_manual(values = levels(pheno_results$colour), labels = levels(pheno_results$strata)) +
+  ggplot2::scale_color_manual(values = levels(pheno_results$colour), labels = levels(pheno_results$strata)) +
+  ggplot2::scale_shape_manual(values = c(21,22), labels = levels(pheno_results$strata)) +
+  #ggplot2::labs(x = "\nDays since COVID-19 diagnosis", y = "Hazard ratio and 95% confidence interval") +
+  ggplot2::labs(x = "\nWeeks since COVID-19 diagnosis", y = "Hazard ratio and 95% confidence interval") +
   ggplot2::guides(fill=ggplot2::guide_legend(ncol = 4, byrow = TRUE)) +
   ggplot2::theme_minimal() +
   ggplot2::theme(panel.grid.major.x = ggplot2::element_blank(),
@@ -117,5 +121,5 @@ ggplot2::ggplot(data =pheno_results,
   #ggplot2::facet_wrap(event~., ncol = 2)
 
 
-ggplot2::ggsave(paste0("output/figure2_COVID_phenotype_days.png"), height = 297, width = 210, unit = "mm", dpi = 600, scale = 1)
+ggplot2::ggsave(paste0("output/figure2_COVID_phenotype_weeks.png"), height = 297, width = 210, unit = "mm", dpi = 600, scale = 1)
 
