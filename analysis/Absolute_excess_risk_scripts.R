f989bb53726a1c0283b20e5255280ec16275749d
tomsrenin <renin.toms@bristol.ac.uk>
2022-02-16 15:51:12 +0000

    Update Absolute_excess_risk_scripts.R

--- analysis/Absolute_excess_risk_scripts.R
+++ analysis/Absolute_excess_risk_scripts.R
@@ -2,36 +2,34 @@
 #Branch:Absolute excess risk calculations
 #Scripts: Renin Toms, Xiyun Jiang, Venexia Walker
 
-library(purrr)
-library(data.table)
-library(tidyverse)
-
-#Import data
-hr_files=list.files(path = "output", pattern = "compiled_HR_results_*")
-hr_files=paste0("output/",hr_files)
-hr_file_paths <- purrr::pmap(list(hr_files),
-                      function(fpath){
-                        df <- fread(fpath)
-                        return(df)
-                      })
-df=rbindlist(hr_file_paths, fill=TRUE)
-
-#Preprocess the AER input data
-input2 <- subset(df, df$term == "days0_14" |
+excess_risk <- function(event, cohort, subgroup, model) {
+  
+  library(purrr)
+  library(data.table)
+  library(tidyverse)
+  
+  #Import data
+  hr_files=list.files(path = "output", pattern = "compiled_HR_results_*")
+  hr_files=paste0("output/",hr_files)
+  hr_file_paths <- purrr::pmap(list(hr_files),
+                               function(fpath){
+                                 df <- fread(fpath)
+                                 return(df)
+                               })
+  df=rbindlist(hr_file_paths, fill=TRUE)
+  
+  #Preprocess the AER input data
+  input2 <- subset(df, df$term == "days0_14" |
                      df$term == "days14_28" |
                      df$term == "days28_56" |
                      df$term == "days56_84" |
                      df$term == "days84_197"|
                      df$term == "days0_28"|
                      df$term == "days28_197") # RT/RK check
-
-input2 <- input2 %>% select(-conf.low, -conf.high, -std.error, -robust.se, -P, -covariates_removed, -cat_covars_collapsed)
-
-excess_risk <- function(event, cohort, subgroup, model) {
-  
-   
-  input1 <- readr::read_csv("output/input1_aer.csv") #1.person days
-  input2 <- readr::read_csv("output/input2_aer.csv") #2.unexposed events, 3.total cases, 4.hr
+  input2 <- input2 %>% select(-conf.low, -conf.high, -std.error, 
+                              -robust.se, -P, -covariates_removed, -cat_covars_collapsed)
+  #input1 <- readr::read_csv("output/input1_aer.csv") #1.person days
+  #input2 <- readr::read_csv("output/input2_aer.csv") #2.unexposed events, 3.total cases, 4.hr
   
   #---------------------------------
   # Step1: Extract the required variables
