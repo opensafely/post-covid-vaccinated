21b24e4e87ab94ec03bb2c91f21d17856b06e154
RochelleKnight <rochelle.knight@bristol.ac.uk>
2022-02-18 12:30:51 +0000

    Create supp table 1

--- analysis/Supplementary_table_1.R
+++ analysis/Supplementary_table_1.R
@@ -6,10 +6,11 @@ library(utils)
 library(data.table)
 library(purrr)
 
-args = commandArgs(trailingOnly=TRUE)
-project = args[[1]]
 
+#-----------------------Determine active outcome events-------------------------
 active_analyses <- read_rds("output/active_analyses.rds")
+events <- active_analyses %>% filter(active=="TRUE")%>%select(outcome,outcome_variable)
+events$outcome_variable <- gsub("out_date_","",events$outcome_variable)
 
 #--------Load fully adjusted main and COVID phenotype results-------------------
 hr_files=list.files(path = "output", pattern = "compiled_HR_results_*")
@@ -22,179 +23,94 @@ hr_file_paths <- pmap(list(hr_files),
                             return(df)
                           })
 combined_hr <- rbindlist(hr_file_paths, fill=TRUE)
-combined_event_counts <- combined_hr %>% select(expo_week,events_total,event,subgroup,model,cohort)
 
-# Select relevant 'term' rows for post-COVID time periods---------------------
+#-------------------------Filter to active outcomes-----------------------------
+combined_hr <- combined_hr %>% filter(event %in% events$outcome_variable)
+combined_hr <- combined_hr %>% filter(subgroup %in% c("main","covid_pheno_hospitalised","covid_pheno_non_hospitalised","covid_history"))
+combined_hr$type <- "Hazard ratio"
+combined_event_counts <- combined_hr %>% select(expo_week,events_total,event,subgroup,model,cohort)
+combined_event_counts$type <- "Event count"
+
+#---------Select relevant 'term' rows for post-COVID time periods---------------
 
 combined_hr <- combined_hr %>% filter(str_detect(term, "^days"))
+combined_event_counts <- combined_event_counts %>% filter(!is.na(expo_week) & str_detect(expo_week, "^days"))
 
 # Make names 'nice' ----------------------------------------------------------
+
+combined_hr <- combined_hr %>% left_join(events, by=c("event"="outcome_variable"))
+combined_event_counts <- combined_event_counts %>% left_join(events, by=c("event"="outcome_variable"))
+
 
 df=list(combined_hr,combined_event_counts)
 
-event_name=lapply(df, function(x) {
-  x$event <- ifelse(x$event=="ami","Acute myocardial infarction",x$event)
-  x$event <- ifelse(x$event=="tia","Transient ischaemic attack",x$event)
-  x$event <- ifelse(x$event=="dvt","Deep vein thrombosis",x$event)
-  x$event <- ifelse(x$event=="hf","Heart failure",x$event)
-  x$event <- ifelse(x$event=="stroke_isch","Ischaemic stroke",x$event)
-  x$event <- ifelse(x$event=="angina","Angina",x$event)
-  x$event <- ifelse(x$event=="vte","Venous thromboembolism",x$event)
-  x$event <- ifelse(x$event=="pe","Pulmonary embolism",x$event)
-  x$event <- ifelse(x$event=="stroke_sah_hs","Subarachnoid haemorrhage and haemorrhagic stroke",x$event)
-  x$event <- ifelse(x$event=="ate","Arterial thromboses",x$event)
+subgroup_name=lapply(df, function(x) {
+  x$subgroup <- ifelse(x$subgroup=="main" & x$model=="mdl_max_adj","All",x$subgroup )
+  x$subgroup <- ifelse(x$subgroup=="main" & x$model=="mdl_agesex","All, age/sex adjusted",x$subgroup )
+  x$subgroup <- ifelse(x$subgroup=="covid_pheno_hospitalised" & x$model=="mdl_max_adj","Hospitalised COVID-19",x$subgroup )
+  x$subgroup <- ifelse(x$subgroup=="covid_pheno_hospitalised" & x$model=="mdl_agesex","Hospitalised COVID-19, age/sex adjusted",x$subgroup )
+  x$subgroup <- ifelse(x$subgroup=="covid_pheno_non_hospitalised" & x$model=="mdl_max_adj","Non-hospitalised COVID-19",x$subgroup )
+  x$subgroup <- ifelse(x$subgroup=="covid_pheno_non_hospitalised" & x$model=="mdl_agesex","Non-hospitalised COVID-19, age/sex adjusted",x$subgroup )
+  x$subgroup <- ifelse(x$subgroup=="covid_history" & x$model=="mdl_max_adj","Prior history of COVID-19",x$subgroup )
+  x$subgroup <- ifelse(x$subgroup=="covid_history" & x$model=="mdl_agesex","Prior history of COVID-19, age/sex adjusted",x$subgroup )
   
-} )
-
-strata_name=lapply(df, function(x) {
-  x$strata <- ifelse(x$strata=="main_main" & x$model=="mdl_max_adj" & x$covid_history=="covid_history_false","Maximally adjusted",x$strata )
-  x$strata <- ifelse(x$strata=="main_main" & x$model=="mdl_agesex" & x$covid_history=="covid_history_false","Age/sex adjusted",x$strata )
-  x$strata <- ifelse(x$strata=="main_main" & x$model=="mdl_max_adj" & x$covid_history=="covid_history_true","Prior history of COVID",x$strata )
-  x$strata <- ifelse(x$strata=="expo_pheno_hospitalised","Hospitalised COVID-19",x$strata )
-  x$strata <- ifelse(x$strata=="expo_pheno_non_hospitalised","Non-hospitalised COVID-19",x$strata )
+  
 })
 
-combined_HR$tidy_event=event_name[[1]]
-combined_event_counts$tidy_event=event_name[[2]]
+combined_hr$tidy_subgroup=subgroup_name[[1]]
+combined_event_counts$tidy_subgroup=subgroup_name[[2]]
 
-combined_HR$tidy_strata=strata_name[[1]]
-combined_event_counts$tidy_strata=strata_name[[2]]
-
-df_HR=combined_HR
 
 # Specify estimate format ----------------------------------------------------  
 
-df_HR$est <- paste0(ifelse(df_HR$estimate>=10,sprintf("%.1f",df_HR$estimate),sprintf("%.2f",df_HR$estimate)),
-                    " (",ifelse(df_HR$conf.low>=10,sprintf("%.1f",df_HR$conf.low),sprintf("%.2f",df_HR$conf.low)),
-                    "-",ifelse(df_HR$conf.high>=10,sprintf("%.1f",df_HR$conf.high),sprintf("%.2f",df_HR$conf.high)),")")
+combined_hr$est <- paste0(ifelse(combined_hr$estimate>=10,sprintf("%.1f",combined_hr$estimate),sprintf("%.2f",combined_hr$estimate)),
+                    " (",ifelse(combined_hr$conf.low>=10,sprintf("%.1f",combined_hr$conf.low),sprintf("%.2f",combined_hr$conf.low)),
+                    "-",ifelse(combined_hr$conf.high>=10,sprintf("%.1f",combined_hr$conf.high),sprintf("%.2f",combined_hr$conf.high)),")")
 
 # Remove unnecessary variables -----------------------------------------------
-df_HR <- df_HR %>% select(term,tidy_event,tidy_strata,est,type)
-df_counts <- combined_event_counts %>% select(expo_week, events_total, tidy_event, tidy_strata,type)
+combined_hr <- combined_hr %>% select(term,outcome,tidy_subgroup,est,model,cohort, type)
+combined_event_counts <- combined_event_counts %>% select(outcome, expo_week, events_total, tidy_subgroup,model,cohort,type)
 
 # Convert long to wide -------------------------------------------------------
 
-df_HR <- tidyr::pivot_wider(df_HR, names_from = term, values_from = est)
-df_counts <- tidyr::pivot_wider(df_counts, names_from = expo_week, values_from = events_total)
+combined_hr <- tidyr::pivot_wider(combined_hr, names_from = term, values_from = est)
+combined_event_counts <- tidyr::pivot_wider(combined_event_counts, names_from = expo_week, values_from = events_total)
 
-# Combine hazard ratios and event counts
-df_HR_counts <- rbind(df_HR,df_counts)
+# Combine hazard ratios and event counts ---------------------------------------
+combined_hr_counts <- rbind(combined_hr,combined_event_counts)
 
-# Specify estimate order -----------------------------------------------------
-
-df_HR_counts$tidy_event <- factor(df_HR_counts$tidy_event, levels=c("Acute myocardial infarction",
-                                                                    "Ischaemic stroke",
-                                                                    "Pulmonary embolism",
-                                                                    "Deep vein thrombosis",
-                                                                    "Transient ischaemic attack",
-                                                                    "Subarachnoid haemorrhage and haemorrhagic stroke",
-                                                                    "Heart failure",
-                                                                    "Angina",
-                                                                    "Arterial thromboses",
-                                                                    "Venous thromboembolism")) 
-
-df_HR_counts$tidy_strata <- factor(df_HR_counts$tidy_strata, levels=c("Maximally adjusted",
-                                                                      "Age/sex adjusted",
-                                                                      "Hospitalised COVID-19",
-                                                                      "Non-hospitalised COVID-19",
-                                                                      "Prior history of COVID")) 
-
-df_HR_counts$type <- factor(df_HR_counts$type, levels = c("HR",
-                                                          "event_count"))
-
-df_HR_counts <- df_HR_counts[order(df_HR_counts$tidy_event, df_HR_counts$tidy_strata,df_HR_counts$type),]
-
-write.csv(df_HR_counts,paste0("output/supplementary_table_1_",project,"_.csv")) <- combined_HR %>% filter(str_detect(term, "^days"))
-combined_event_counts <- combined_event_counts %>% filter(str_detect(expo_week, "^days"))
-
-# Make names 'nice' ----------------------------------------------------------
-
-df=list(combined_HR,combined_event_counts)
-
-event_name=lapply(df, function(x) {
-  x$event <- ifelse(x$event=="ami","Acute myocardial infarction",x$event)
-  x$event <- ifelse(x$event=="tia","Transient ischaemic attack",x$event)
-  x$event <- ifelse(x$event=="dvt","Deep vein thrombosis",x$event)
-  x$event <- ifelse(x$event=="hf","Heart failure",x$event)
-  x$event <- ifelse(x$event=="stroke_isch","Ischaemic stroke",x$event)
-  x$event <- ifelse(x$event=="angina","Angina",x$event)
-  x$event <- ifelse(x$event=="vte","Venous thromboembolism",x$event)
-  x$event <- ifelse(x$event=="pe","Pulmonary embolism",x$event)
-  x$event <- ifelse(x$event=="stroke_sah_hs","Subarachnoid haemorrhage and haemorrhagic stroke",x$event)
-  x$event <- ifelse(x$event=="ate","Arterial thromboses",x$event)
-  
-} )
-
-strata_name=lapply(df, function(x) {
-  x$strata <- ifelse(x$strata=="main_main" & x$model=="mdl_max_adj" & x$covid_history=="covid_history_false","Maximally adjusted",x$strata )
-  x$strata <- ifelse(x$strata=="main_main" & x$model=="mdl_agesex" & x$covid_history=="covid_history_false","Age/sex adjusted",x$strata )
-  x$strata <- ifelse(x$strata=="main_main" & x$model=="mdl_max_adj" & x$covid_history=="covid_history_true","Prior history of COVID",x$strata )
-  x$strata <- ifelse(x$strata=="expo_pheno_hospitalised","Hospitalised COVID-19",x$strata )
-  x$strata <- ifelse(x$strata=="expo_pheno_non_hospitalised","Non-hospitalised COVID-19",x$strata )
-})
-
-combined_HR$tidy_event=event_name[[1]]
-combined_event_counts$tidy_event=event_name[[2]]
-
-combined_HR$tidy_strata=strata_name[[1]]
-combined_event_counts$tidy_strata=strata_name[[2]]
-
-df_HR=combined_HR
-
-# Specify estimate format ----------------------------------------------------  
-
-df_HR$est <- paste0(ifelse(df_HR$estimate>=10,sprintf("%.1f",df_HR$estimate),sprintf("%.2f",df_HR$estimate)),
-                    " (",ifelse(df_HR$conf.low>=10,sprintf("%.1f",df_HR$conf.low),sprintf("%.2f",df_HR$conf.low)),
-                    "-",ifelse(df_HR$conf.high>=10,sprintf("%.1f",df_HR$conf.high),sprintf("%.2f",df_HR$conf.high)),")")
-
-# Remove unnecessary variables -----------------------------------------------
-df_HR <- df_HR %>% select(term,tidy_event,tidy_strata,est,type)
-df_counts <- combined_event_counts %>% select(expo_week, events_total, tidy_event, tidy_strata,type)
-
-# Convert long to wide -------------------------------------------------------
-
-df_HR <- tidyr::pivot_wider(df_HR, names_from = term, values_from = est)
-df_counts <- tidyr::pivot_wider(df_counts, names_from = expo_week, values_from = events_total)
-
-# Combine hazard ratios and event counts
-df_HR_counts <- rbind(df_HR,df_counts)
-
-# Specify estimate order -----------------------------------------------------
-
-df_HR_counts$tidy_event <- factor(df_HR_counts$tidy_event, levels=c("Acute myocardial infarction",
-                                                                    "Ischaemic stroke",
-                                                                    "Pulmonary embolism",
-                                                                    "Deep vein thrombosis",
-                                                                    "Transient ischaemic attack",
-                                                                    "Subarachnoid haemorrhage and haemorrhagic stroke",
-                                                                    "Heart failure",
-                                                                    "Angina",
-                                                                    "Arterial thromboses",
-                                                                    "Venous thromboembolism")) 
-
-df_HR_counts$tidy_strata <- factor(df_HR_counts$tidy_strata, levels=c("Maximally adjusted",
-                                                                      "Age/sex adjusted",
-                                                                      "Hospitalised COVID-19",
-                                                                      "Non-hospitalised COVID-19",
-                                                                      "Prior history of COVID")) 
-
-df_HR_counts$type <- factor(df_HR_counts$type, levels = c("HR",
-                                                          "event_count"))
-
-df_HR_counts <- df_HR_counts[order(df_HR_counts$tidy_event, df_HR_counts$tidy_strata,df_HR_counts$type),]
-
-write.csv(df_HR_counts,paste0("output/supplementary_table_1_",project,"_.csv"))
-
-
-
-  
-  
-  
+# Specify column order ---------------------------------------------------------
+if(length(colnames(combined_hr_counts)[grepl("^days",colnames(combined_hr_counts))]) ==5){
+  combined_hr_counts <- combined_hr_counts %>% select(outcome,tidy_subgroup,cohort,model,type,days0_14,days14_28,days28_56,days56_84,days84_197)
 }else{
-  df=data.frame(ncol=1)
-  colnames(df)="no_results"
-  write.csv(df,paste0("output/supplementary_table_1_",project,"_.csv"))
+  combined_hr_counts <- combined_hr_counts %>% select(outcome,tidy_subgroup,cohort,model,type,days0_14,days14_28,days28_56,days56_84,days84_197,days0_28,days28_197)
 }
+
+# Specify estimate order -------------------------------------------------------
+
+combined_hr_counts$outcome <- factor(combined_hr_counts$outcome, levels=events$outcome) 
+
+combined_hr_counts$tidy_subgroup <- factor(combined_hr_counts$tidy_subgroup, levels=c("All",
+                                                                      "All, age/sex adjusted",
+                                                                      "Hospitalised COVID-19",
+                                                                      "Hospitalised COVID-19, age/sex adjusted",
+                                                                      "Non-hospitalised COVID-19",
+                                                                      "Non-hospitalised COVID-19, age/sex adjusted",
+                                                                      "Prior history of COVID-19",
+                                                                      "Prior history of COVID-19, age/sex adjusted")) 
+
+combined_hr_counts$type <- factor(combined_hr_counts$type, levels = c("Hazard ratio",
+                                                          "Event count"))
+
+combined_hr_counts$cohort <- factor(combined_hr_counts$cohort, levels = c("vaccinated",
+                                                                          "electively_unvaccinated"))
+
+combined_hr_counts <- combined_hr_counts[order(combined_hr_counts$outcome,combined_hr_counts$cohort, combined_hr_counts$tidy_subgroup,combined_hr_counts$type),]
+
+# Save as .csv------------------------------------------------------------------
+write.csv(combined_hr_counts,paste0("output/supplementary_table_1.csv")) 
+
+
 
 
 
