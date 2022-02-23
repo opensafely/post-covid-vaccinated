de6ff70c489dc3d60f5e16acc0064b5580c64393
Yinghui Wei <91749253+yinghuiwei@users.noreply.github.com>
2022-02-23 16:54:34 +0000

    update calculation

--- analysis/table_2.R
+++ analysis/table_2.R
@@ -39,7 +39,7 @@ table_2_output <- function(population){
   
   # record variable names for covariate
   vars_names <- tidyselect::vars_select(names(input), !starts_with(c('sub_','cov_','qa_','vax_cat'), ignore.case = TRUE))
-  vars_names <- c(vars_names, "sub_cat_covid19_hospital")
+  vars_names <- c(vars_names, "sub_cat_covid19_hospital", "exp_date_covid19_confirmed")
   
   # Create a data frame for survival data: to avoid carrying covariates in the calculation
   survival_data <- input[,vars_names] 
@@ -60,7 +60,7 @@ table_2_output <- function(population){
   colnames(table_2) <- col_headings
   table_2$event <- event_names
   
-  summary_stats <- function(population, survival_data, event_dates_names, index)
+  summary_stats <- function(population, infection_subgroup, survival_data, event_dates_names, index)
   {
     survival_data$event_date <- survival_data[,event_dates_names[index]]
     if(population=="vaccinated"){
@@ -70,28 +70,41 @@ table_2_output <- function(population){
       survival_data <- survival_data %>% rowwise() %>% mutate(follow_up_end=  min(vax_date_covid_1,event_date, death_date,cohort_end_date,na.rm = TRUE))
       survival_data <- survival_data %>% dplyr::select(!c(vax_date_covid_1))
     }
+    # exposure date
+    # survival_data$expo_date <- as.Date(ifelse((!is.na(survival_data$event_date)) & 
+    #                                             (survival_data$exp_date_covid19_confirmed < survival_data$event_date), 
+    #                                              survival_data$exp_date_covid19_confirmed, NA), origin='1970-01-01')
+    
     # follow-up days
     survival_data = survival_data %>% mutate(follow_up_period = as.numeric((as.Date(follow_up_end) - as.Date(index_date)))+1) 
     #hist(survival_data$follow_up_period)  ## check if there are any negative follow-up periods
     survival_data = survival_data %>% filter(follow_up_period >=0 & follow_up_period < 197) # filter out follow up period 
     survival_data = survival_data %>% mutate(follow_up_years = follow_up_period / 365.2) # follow-up years
     # check that the event dates are within the follow up period
-    event_count <- length(which(survival_data$event_date >= survival_data$index_date  & survival_data$event_date<= survival_data$follow_up_end))
+    event_count_post_infection <- length(which(survival_data$event_date   >= survival_data$index_date &
+                                                 survival_data$event_date >= survival_data$exp_date_covid19_confirmed & 
+                                                 survival_data$event_date <= survival_data$follow_up_end))
+    event_count_before_infection <- length(which(survival_data$event_date >= survival_data$index_date &
+                                                 survival_data$event_date <  survival_data$exp_date_covid19_confirmed & 
+                                                 survival_data$event_date <= survival_data$follow_up_end))
+    event_count_no_infection <- length(which(survival_data$event_date   >= survival_data$index_date &
+                                             survival_data$event_date <= survival_data$follow_up_end))
+    event_count_no_infection = event_count_no_infection + event_count_before_infection
     person_years_follow_up  = round(sum(survival_data$follow_up_years, na.rm = TRUE),1)
+    event_count = ifelse(infection_subgroup == "no_infection", event_count_no_infection, event_count_post_infection)
     incidence_rate = round(event_count/person_years_follow_up, 4)
-    return(c(event_count, person_years_follow_up, incidence_rate))
+    return(c(event_count,person_years_follow_up, incidence_rate))
   }
   
   for(i in 1:length(event_dates_names)){
-    table_2[i,2:4] <- summary_stats(population, survival_data, event_dates_names, i)
-    table_2[i,5:7] <- summary_stats(population, survival_data[survival_data$sub_cat_covid19_hospital=="no_infection",], event_dates_names, i)
-    table_2[i,8:10] <- summary_stats(population, survival_data[survival_data$sub_cat_covid19_hospital=="non_hospitalised",], event_dates_names, i)
-    table_2[i,11:13] <- summary_stats(population, survival_data[survival_data$sub_cat_covid19_hospital=="hospitalised",], event_dates_names, i)
-    
+   # table_2[i,2:4] <- summary_stats(population, survival_data, event_dates_names, i)
+    table_2[i,2:4] <- summary_stats(population, "no_infection", survival_data[survival_data$sub_cat_covid19_hospital=="no_infection",], event_dates_names, i)
+    table_2[i,5:7] <- summary_stats(population, "non_hospitalised",survival_data[survival_data$sub_cat_covid19_hospital=="non_hospitalised",], event_dates_names, i)
+    table_2[i,8:10] <- summary_stats(population, "hospitalised", survival_data[survival_data$sub_cat_covid19_hospital=="hospitalised",], event_dates_names, i)
   }
-  names(table_2)[5:7] <- c("no_infection_sub_event_count", "no_infection_sub_person_yrs_fp", "no_infection_sub_incidence_rate")
-  names(table_2)[8:10] <- c("non_hospitalised_sub_event_count", "non_hospitalised_sub_person_yrs_fp", "non_hospitalised_sub_incidence_rate")
-  names(table_2)[11:13] <- c("hospitalised_sub_event_count", "hospitalised_sub_person_yrs_fp", "hospitalised_sub_incidence_rate")
+  names(table_2)[2:4] <- c("no_infection_sub_event_count", "no_infection_sub_person_yrs_fp", "no_infection_sub_incidence_rate")
+  names(table_2)[5:7] <- c("non_hospitalised_sub_event_count", "non_hospitalised_sub_person_yrs_fp", "non_hospitalised_sub_incidence_rate")
+  names(table_2)[8:10] <- c("hospitalised_sub_event_count", "hospitalised_sub_person_yrs_fp", "hospitalised_sub_incidence_rate")
   
   write.csv(table_2, file= paste0("output/", "table2_", population, ".csv"), row.names = F)
 }
@@ -104,3 +117,5 @@ if(population == "both"){
 }else{
   table_2_output(population)
 }
+
+
