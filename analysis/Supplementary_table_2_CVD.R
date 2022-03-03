e53db238282a7295ed78edb0241440c58074212b
RochelleKnight <rochelle.knight@bristol.ac.uk>
2022-03-03 16:05:40 +0000

    Update Supplementary_table_2_CVD.R

--- analysis/Supplementary_table_2_CVD.R
+++ analysis/Supplementary_table_2_CVD.R
@@ -8,11 +8,8 @@ library(purrr)
 library(markdown)
 
 #-----------------------Determine active outcome events-------------------------
-active_analyses <- read_rds("output/active_analyses.rds")
+active_analyses <- read_rds("lib/active_analyses.rds")
 active_analyses$outcome_variable <- gsub("out_date_","",active_analyses$outcome_variable)
-#cols=colnames(active_analyses)[grepl("active|agegp|ethnicity|prior_history_TRUE|prior_history_FALSE",colnames(active_analyses))]
-#events <- active_analyses %>% filter_at(vars(cols), all_vars(.==TRUE))
-#events$outcome_variable <- gsub("out_date_","",events$outcome_variable)
 
 #--------Load HR results-------------------
 hr_files=list.files(path = "output", pattern = "compiled_HR_results_*")
@@ -30,22 +27,16 @@ combined_hr <- rbindlist(hr_file_paths, fill=TRUE)
 
 #-------------------------Filter to active outcomes-----------------------------
 combined_hr <- combined_hr %>% filter(event %in% c("ate","vte"))
-#combined_hr$type <- "Hazard ratio"
-#combined_event_counts <- combined_hr %>% select(expo_week,events_total,event,subgroup,model,cohort)
-#combined_event_counts$type <- "Event count"
 
 #---------Select relevant 'term' rows for post-COVID time periods---------------
 
 combined_hr <- combined_hr %>% filter(str_detect(term, "^days"))
-#combined_event_counts <- combined_event_counts %>% filter(!is.na(expo_week) & str_detect(expo_week, "^days"))
 
 # Make names 'nice' ----------------------------------------------------------
 
 combined_hr <- combined_hr %>% left_join(active_analyses %>% select(outcome, outcome_variable), by=c("event"="outcome_variable"))
-#combined_event_counts <- combined_event_counts %>% left_join(events %>% select(outcome, outcome_variable), by=c("event"="outcome_variable"))
 
 df=list(combined_hr)
-#df=list(combined_hr,combined_event_counts)
 
 subgroup_name=lapply(df, function(x) {
   x$subgroup <- ifelse(x$subgroup=="main" & x$model=="mdl_max_adj","All",x$subgroup )
@@ -87,7 +78,6 @@ subgroup_name=lapply(df, function(x) {
 })
 
 combined_hr$tidy_subgroup=subgroup_name[[1]]
-#combined_event_counts$tidy_subgroup=subgroup_name[[2]]
 
 # Specify estimate format ----------------------------------------------------  
 
@@ -97,15 +87,10 @@ combined_hr$est <- paste0(ifelse(combined_hr$estimate>=10,sprintf("%.1f",combine
 
 # Remove unnecessary variables -----------------------------------------------
 combined_hr <- combined_hr %>% select(term,outcome,tidy_subgroup,est,model,cohort)
-#combined_event_counts <- combined_event_counts %>% select(outcome, expo_week, events_total, tidy_subgroup,model,cohort,type)
 
 # Convert long to wide -------------------------------------------------------
 
 combined_hr <- tidyr::pivot_wider(combined_hr, names_from = term, values_from = est)
-#combined_event_counts <- tidyr::pivot_wider(combined_event_counts, names_from = expo_week, values_from = events_total)
-
-# Combine hazard ratios and event counts ---------------------------------------
-#combined_hr_counts <- rbind(combined_hr,combined_event_counts)
 
 # Specify column order ---------------------------------------------------------
 #if(length(colnames(combined_hr_counts)[grepl("^days",colnames(combined_hr_counts))]) ==5){
@@ -157,42 +142,14 @@ combined_hr$tidy_subgroup <- factor(combined_hr$tidy_subgroup, levels=c("All",
                                                                         )) 
 
                                                                                       
-
-
-#combined_hr$type <- factor(combined_hr$type, levels = c("Hazard ratio",
-#                                                        "Event count"))
-
 combined_hr$cohort <- factor(combined_hr$cohort, levels = c("vaccinated",
                                                             "electively_unvaccinated"))
 
 combined_hr <- combined_hr[order(combined_hr$cohort, combined_hr$outcome, combined_hr$tidy_subgroup),]
 
-#time_periods <- colnames(combined_hr_counts)[grepl("^days",colnames(combined_hr_counts))]
-#df <- combined_hr_counts %>% 
-#  filter(type=="Event count") %>% 
-#  mutate_at(vars(time_periods), ~as.numeric(.)) %>% 
-#  filter_at(vars(time_periods), any_vars(.<5))
-
 # Save as .csv------------------------------------------------------------------
+
 for(i in c("vaccinated","electively_unvaccinated")){
   df <- combined_hr %>% filter(cohort == i)
   write_csv(df,paste0("output/supplementary_table_2_",i,".csv")) 
 }
-
-#write_csv(df,paste0("output/less_than_5_events_supplementary_table_2.csv")) 
-
-# Save a html of the results for easy reading in Level 4 server-----------------
-
-#rmarkdown::render("analysis/supplementary_table_2.Rmd",output_file="supplementary_table_2",output_dir="output")
-#rmarkdown::render("analysis/less_than_5_events_supplementary_table_2.Rmd",output_file="less_than_5_events_supplementary_table_2",output_dir="output")
-
-
-
-
-
-
-
-
-
-
-
