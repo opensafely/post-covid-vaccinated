f1bc4615380b903200f771e5d6a6d8fb5c06ca31
tomsrenin <renin.toms@bristol.ac.uk>
2022-03-07 10:06:22 +0000

    Update Absolute_excess_risk_scripts.R

--- analysis/Absolute_excess_risk_scripts.R
+++ analysis/Absolute_excess_risk_scripts.R
@@ -2,13 +2,13 @@
 #Branch:Absolute excess risk calculations
 #Scripts: Renin Toms, Xiyun Jiang, Venexia Walker
 
-#Prephasic use to check the function
-cohort <- "vaccinated" 
-fit <- "mdl_max_adj"
-subgroup <- "prior_history_FALSE"
-model <- "mdl_max_adj"
+#use only to check the function
+#group <- "vaccinated" 
+#fit <- "mdl_max_adj"
+#outcome <- "vte" 
+#strata <- "prior_history_FALSE"
 
-
+#Create the function
 excess_risk <- function(event, cohort, subgroup, model) {
   
   #Call the library
@@ -30,54 +30,63 @@ excess_risk <- function(event, cohort, subgroup, model) {
                         })
   input2=rbindlist(input2, fill=TRUE)
   
-  #Preprocess the AER input data
-  input2 <- input2 %>% select(-conf.low, -conf.high, -std.error, -robust.se, -P, -covariates_removed, -cat_covars_collapsed)
+  #Preprocess the input data (can be reduced when tuned in with real data inputs)
+  input2 <- input2 %>% select(-conf.low, -conf.high, -std.error,-robust.se, -P, -covariates_removed, -cat_covars_collapsed)
   input2 <- input2 %>% filter(term == "days0_14" |
                                 term == "days14_28" |
                                 term == "days28_56" |
                                 term == "days56_84" |
                                 term == "days84_197"|
                                 term == "days0_28"|
-                                term == "days28_197") # RT/RK check
+                                term == "days28_197")
+  input1$strata[input1$strata =="sex_M"] <- "sex_Male"
+  input1$strata[input1$strata =="sex_F"] <- "sex_Female"
+  
+  input1$strata[input1$strata =="ethnicity_1"] <- "ethnicity_White"
+  input1$strata[input1$strata =="ethnicity_2"] <- "ethnicity_Black"
+  input1$strata[input1$strata =="ethnicity_3"] <- "ethnicity_South_Asian"
+  input1$strata[input1$strata =="ethnicity_4"] <- "ethnicity_Mixed"
+  input1$strata[input1$strata =="ethnicity_5"] <- "ethnicity_Other"
+  input1$strata[input1$strata =="ethnicity_6"] <- "ethnicity_Missing"
+  
+  input1$strata[input1$strata =="prior_history_true"] <- "prior_history_TRUE"
+  input1$strata[input1$strata =="prior_history_false"] <- "prior_history_FALSE"
   #--------------------------------------
   # Step1: Extract the required variables
   #--------------------------------------
   #1. Person days
-  fp_person_days <- input1[input1$event == event & input1$model == model &
-                             input1$cohort == cohort & input1$strata == subgroup,]$person_days #RT/RK/VW -check L
-  
+  fp_person_days <- input1[input1$event == outcome & input1$model == fit  &
+                             input1$cohort == group & input1$strata == strata,]$person_days
   #2.unexposed events
-  unexposed_events <-  input2[input2$event == event & input2$model == model & 
-                                input2$cohort == cohort & input2$subgroup == subgroup & 
-                                input2$expo_week== "pre expo",]$events_total
-  
+  unexposed_events <- input2[input2$event == outcome & input2$model == fit  & 
+                               input2$cohort == group & input2$subgroup == strata & 
+                               input2$expo_week== "pre expo",]$events_total
   #3.Total cases
-  total_cases <-  input2[input2$event == event & input2$model == model & 
-                           input2$cohort == cohort & input2$subgroup == subgroup & 
+  total_cases <-  input2[input2$event == outcome & input2$model == fit  & 
+                           input2$cohort == group & input2$subgroup == strata & 
                            input2$expo_week== "pre expo",]$total_covid19_cases
-  
   #4.locate the estimates
   #0-14 days
-  hr_14 <- input2[input2$event == event & input2$model == model & 
-                    input2$cohort == cohort & input2$subgroup == subgroup & input2$term == "days0_14",]$estimate
+  hr_14 <- input2[input2$event == outcome  & input2$model == fit  & 
+                    input2$cohort == group & input2$subgroup == strata & input2$term == "days0_14",]$estimate
   #14-28 days
-  hr_28 <- input2[input2$event == event & input2$model == model & 
-                    input2$cohort == cohort & input2$subgroup == subgroup & input2$term == "days14_28",]$estimate
+  hr_28 <- input2[input2$event == outcome & input2$model == fit  & 
+                    input2$cohort == group & input2$subgroup == strata& input2$term == "days14_28",]$estimate
   #28-56 days
-  hr_56 <- input2[input2$event == event & input2$model == model & 
-                    input2$cohort == cohort & input2$subgroup == subgroup & input2$term == "days28_56",]$estimate
+  hr_56 <- input2[input2$event == outcome & input2$model == fit  & 
+                    input2$cohort == group & input2$subgroup == strata& input2$term == "days28_56",]$estimate
   #56-84 days
-  hr_84 <- input2[input2$event == event & input2$model == model & 
-                    input2$cohort == cohort & input2$subgroup == subgroup & input2$term == "days56_84",]$estimate
+  hr_84 <- input2[input2$event == outcome & input2$model == fit  & 
+                    input2$cohort == group & input2$subgroup == strata& input2$term == "days56_84",]$estimate
   #84-196 days
-  hr_196 <- input2[input2$event == event & input2$model == model & 
-                     input2$cohort == cohort & input2$subgroup == subgroup & input2$term == "days84_197",]$estimate
+  hr_196 <- input2[input2$event == outcome & input2$model == fit  & 
+                     input2$cohort == group & input2$subgroup == strata& input2$term == "days84_197",]$estimate
   #Alternative 0-28 days
-  hr0_28 <- input2[input2$event == event & input2$model == model & 
-                     input2$cohort == cohort & input2$subgroup == subgroup & input2$term == "days0_28",]$estimate
-  #Alternative 28 - 196 days
-  hr28_196<- input2[input2$event == event & input2$model == model & 
-                      input2$cohort == cohort & input2$subgroup == subgroup & input2$term == "days28_196",]$estimate
+  hr0_28 <- input2[input2$event == outcome  & input2$model == fit  & 
+                     input2$cohort == group & input2$subgroup == strata& input2$term == "days0_28",]$estimate
+  #Alternative 28_196 days
+  hr28_196 <- input2[input2$event == outcome  & input2$model == fit  & 
+                       input2$cohort == group & input2$subgroup == strata& input2$term == "days28_197",]$estimate
   #--------------------------------------------------------------------
   #Step2.Calculate the average daily CVD incidence   - in the unexposed
   #--------------------------------------------------------------------
@@ -90,10 +99,10 @@ excess_risk <- function(event, cohort, subgroup, model) {
   #Description:Use a life table approach to calculate age- and sex specific cumulative risks over time, - with and without COVID-19. 
   lifetable <- data.frame(c(1:196))
   colnames(lifetable) <- c("days")
-  lifetable$event <- event
-  lifetable$model <- model
-  lifetable$cohort <- cohort
-  lifetable$subgroup <- subgroup 
+  lifetable$event <- outcome
+  lifetable$model <- fit
+  lifetable$cohort <- group
+  lifetable$subgroup <- strata 
   
   lifetable$q <- incidence_rate 
   lifetable$'1-q' <- 1 - lifetable$q 
@@ -105,7 +114,7 @@ excess_risk <- function(event, cohort, subgroup, model) {
   #Description: Multiply  the average daily incidence by the maximally adjusted age- and sex-specific HR, -
   # for that day to derive the incidence on each day after COVID-19. 
   
-  #assign the hr estimates
+  #1.assign the hr estimates
   lifetable$h <- ifelse(lifetable$days < 15, rep(hr_14),0)
   lifetable$h <- ifelse(lifetable$days > 14 & lifetable$days < 29, rep(hr_28),lifetable$h)
   lifetable$h <- ifelse(lifetable$days < 29 & is.na(lifetable$h), rep(hr0_28),lifetable$h)#alternative for 0-28 days
@@ -113,10 +122,16 @@ excess_risk <- function(event, cohort, subgroup, model) {
   lifetable$h <- ifelse(lifetable$days > 28 & lifetable$days < 57, rep(hr_56),lifetable$h)
   lifetable$h <- ifelse(lifetable$days > 56 & lifetable$days < 85, rep(hr_84),lifetable$h)
   lifetable$h <- ifelse(lifetable$days > 84 & lifetable$days < 197, rep(hr_196),lifetable$h)
-  lifetable$h <- ifelse(lifetable$days > 28 & lifetable$days < 197 & is.na(lifetable$h), rep(hr28_196),lifetable$h)#alternative for 28-196 days
+  lifetable$h <- ifelse(lifetable$days > 28 & lifetable$days < 197 & is.na(lifetable$h), rep(hr28_196),lifetable$h)
+  #alternative for 28-196 days
   
+  #2.assign qh
   lifetable$qh <- lifetable$q*lifetable$h
+  
+  #3.assign 1-qh
   lifetable$'1-qh' <- 1 - lifetable$qh
+  
+  #4.assign sc
   lifetable$sc <- cumprod(lifetable$`1-qh`)
   #-----------------------------------------
   #Step5. Calculate the Absolute excess risk
@@ -124,78 +139,124 @@ excess_risk <- function(event, cohort, subgroup, model) {
   #Description:Subtract the latter from the former to derive the absolute excess risks over time after COVID-19, -
   #compared with no COVID-19 diagnosis. 
   
-  #AER = Sc-S=difference in absolute risk
-  lifetable$AER <- lifetable$sc - lifetable$s
-  #AER on day 196 
-  AER_196 <- lifetable[nrow(lifetable),]$AER * total_cases
+  #1.AER =difference in absolute risk
+  lifetable$'s-sc' <- lifetable$s - lifetable$sc
+  
+  #2.AER on day 196 
+  AER_196 <- lifetable[nrow(lifetable),]$'s-sc' * total_cases
   print(AER_196) # 183.7275
   # 184 excess 'events' happens 196 days after 7558 total covid19 'cases'.
   
-  
+
   # Return results
-  results <- data.frame(event = event,
-                        cohort = cohort,
-                        subgroup = subgroup,
-                        model = model,
-                        AER_196 = lifetable[nrow(lifetable),]$AER * total_cases,
+  AER_all_results <- data.frame(event = outcome,
+                        cohort = group,
+                        subgroup = strata,
+                        model = fit,
+                        AER_196 = lifetable[nrow(lifetable),]$'s-sc' * total_cases,
                         stringsAsFactors = FALSE)
   
-  return(results) 
+  return(AER_all_results) 
+  
   
 }
 
-##########################
-#plotting
-######life table##########
-#convert to AER%
-lifetable$AER_p <- lifetable$AER*100
-plot(lifetable$days, lifetable$AER_p)
+#---------------------------------------------------------
+#Step6. Run the function now---FOR the active analyses----
+#--------------------------------------------------------
+#1. Define the active analyses
+active <- readr::read_rds("lib/active_analyses.rds")
+active <- active[active$active==TRUE,]
+active$event <- gsub("out_date_","",active$outcome_variable)
+active[,c("active","outcome","outcome_variable","prior_history_var","covariates")] <- NULL
 
-p_line<-ggplot(lifetable,
-               aes(x=days,
-                   y=AER_p,
-                   group=1)) +
-  #geom_errorbar(aes(ymin=incidence_rate_difference_LB, ymax=incidence_rate_difference_UB), width=.2,
-  #              position=position_dodge(.9))+
-  geom_line(size=1.5)+
-  #geom_point()+
-  scale_x_continuous(breaks = c(0,20,40,60,80,100,120,140,160,180,200),limits = c(0,200))+
-  scale_y_continuous(limits = c(0,4))+
-  labs(x='days since COVID-19 diagnosis',y='Cumulative difference in absolute risk  (%)',
-       title = 'AMI')+
-  theme(plot.title = element_text(hjust = 0.5))
+active <- tidyr::pivot_longer(active, 
+                              cols = setdiff(colnames(active),c("event","model","cohort")), 
+                              names_to = "strata")
 
-p_line
+active <- active[active$value==TRUE, c("event","model","cohort","strata")]
 
-# RT - add return the life table for - 'ate' and 'vte' for plotting
-#Subgroups - 2. VACCINATED AND ELECTIVELY UNVACCINATED
+active$model <- ifelse(active$model=="all","mdl_agesex;mdl_max_adj",active$model)
+active <- tidyr::separate_rows(active, model, sep = ";")
 
-#1.column1 - 196 days
-#column2 - Cumulative AER(%)
+active$cohort <- ifelse(active$cohort=="all","vaccinated;electively_unvaccinated",active$cohort)
+active <- tidyr::separate_rows(active, cohort, sep = ";")
 
-              #2.AERp_ate_all
+#active$group <- gsub("_.*","",active$strata)
+#active$group <- ifelse(active$group=="covid" & grepl("covid_history",active$strata), "covid_history", active$group)
+#active$group <- ifelse(active$group=="covid" & grepl("covid_pheno",active$strata), "covid_pheno", active$group)
+#active$group <- ifelse(active$group=="prior" & grepl("prior_history",active$strata), "prior_history", active$group)
+#active <- unique(active[,c("event","model","cohort","group")])
 
-              #3.AERp_ate_male
-              #4.AERp_ate_female 
+#Preprocess to right outcomes, names and order
+active <- active[active$event %in% c("ate", "vte") & active$model %in% c("mdl_max_adj"),]
 
-              #5.AERp_ate_18_39
-              #6.AERp_ate_40-59
-              #7.AERp_ate_60-79
-              #8.AERp_ate_80-110
+colnames(active)[colnames(active) == 'group'] <- 'strata'
+colnames(active)[colnames(active) == 'cohort'] <- 'group'
+colnames(active)[colnames(active) == 'model'] <- 'fit'
+colnames(active)[colnames(active) == 'event'] <- 'outcome'
 
-              #9.AERp_ate_asian              ##additionals??
-              #10.AERp_ate_black
-              #11.AERp_ate_white
-              #12.AERp_ate_mixed
-              #13.AERp_ate_others
+active <- active %>% select(-outcome,-fit, everything())
+active <- active %>% select(-strata,-outcome, everything())
+active <- active %>% select(-strata, everything())
+active <- active %>% select(-strata, everything())
 
-              #14.AERp_ate_history
-              #15.AERp_ate_no_history
+active <- active[-32,]
+active <- active[-66,]
+active <- active[-66,]
 
-              #16.AERp_ate_hospitalised
-              #17.AERp_ate_not_hospitalised
+#group <- "vaccinated" # cohort
+#fit <- "mdl_max_adj" #model
+#outcome <- "ate" # event
+#strata <- "sex_Female" # subgroup
 
-excess_risk(event <- "ate",
-            cohort <- "vaccinated",
-            subgroup <- "main",
-            model <- "mdl_max_adj")
+#table(active$strata)
+#table(input2$subgroup)
+
+
+
+for (i in 1:nrow(active)) {
+  
+  #files <- list.files(path = "output", pattern = "lifetable_*")
+  #files <- files[grepl(active$strata[i])]
+  
+  figure4_tbl(active$group[i],active$fit[i],active$outcome[i],active$strata[i])
+  
+  #input2- Import data
+  lt_files=list.files(path = "output", pattern = "lifetable_delta_*")
+  lt_files=lt_files[endsWith(lt_files,".csv")]
+  lt_files=paste0("output/",lt_files)
+  f4_compiled_lifetables <- purrr::pmap(list(lt_files),
+                                        function(fpath){
+                                          df <- fread(fpath)
+                                          return(df)
+                                        })
+  f4_compiled_lifetables=rbindlist(f4_compiled_lifetables, fill=TRUE)
+  write.csv(f4_compiled_lifetables, paste0("output/Figure4_compiled_lifetables.csv"), row.names = F)
+}
+
+#Delete file if it exists
+if (file.exists(lt_files)) { file.remove(lt_files)}
+
+69*196
+
+
+
+
+
+
+
+
+
+
+
+
+
+#-------------------------------------------
+#Step7. Output1 the csv
+#-------------------------------------------
+
+write.csv(AER_all_results, ("output/AER_all_results.csv"), row.names = F)
+
+
+
