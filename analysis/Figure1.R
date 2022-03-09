907a37759c4fa2c479e1155fc69c8ce8e195a44f
Genevieve Cezard <gic30@medschl.cam.ac.uk>
2022-03-09 13:49:51 +0000

    Update Figure1 code post-review

--- analysis/Figure1.R
+++ analysis/Figure1.R
@@ -52,9 +52,6 @@ main_estimates <- subset(estimates, subgroup == "main" & event %in% outcome_to_p
 #--------------------------------------#
 # 4. Specify time in weeks (mid-point) #
 #--------------------------------------#
-# Select estimates for time periods only
-main_estimates <- main_estimates %>% dplyr::filter(str_detect(term, "^days"))
-
 term=unique(main_estimates$term)
 #class(term)
 #term=as.character()
@@ -78,6 +75,19 @@ for(i in 1:(length(cuts)-2)){
 }
 time=append(time,((cuts[length(cuts)]-1)+cuts[length(cuts)-1])/14) # change 197 to 196 i.e. cuts[length(cuts)]-1 for the last time
 
+# Add time in weeks for the reduced time periods at the end
+time=append(time,2) 
+time=append(time,((cuts[length(cuts)]-1)+28)/14)
+
+# Create a character vector of all the time period terms in increasing order with the reduced time periods at the end
+term=as.character()
+for(i in 1:(length(cuts)-1)){
+  term=append(term,as.character(paste0("days",cuts[i],"_",cuts[i+1])))
+}
+term=append(term,as.character("days0_28"))
+term=append(term,as.character(paste0("days28_",cuts[length(cuts)])))
+
+# Get time and term into a data frame to link
 term_to_time <- data.frame(term = term, time = time)
 
 # Add time to estimates dataset 
@@ -88,12 +98,12 @@ main_estimates <- merge(main_estimates, term_to_time, by = c("term"), all.x = TR
 #------------------------------------------#
 # Specify colours
 main_estimates$colour <- ""
-main_estimates$colour <- ifelse(main_estimates$model=="mdl_agesex","#000000",main_estimates$colour) # Black
-main_estimates$colour <- ifelse(main_estimates$model=="mdl_max_adj","#bababa",main_estimates$colour) # Grey
+main_estimates$colour <- ifelse(main_estimates$model=="mdl_agesex","#bababa",main_estimates$colour) # Grey
+main_estimates$colour <- ifelse(main_estimates$model=="mdl_max_adj","#000000",main_estimates$colour) # Black
 
 # Factor variables for ordering
 main_estimates$model <- factor(main_estimates$model, levels=c("mdl_agesex", "mdl_max_adj")) 
-main_estimates$colour <- factor(main_estimates$colour, levels=c("#000000", "#bababa"))
+main_estimates$colour <- factor(main_estimates$colour, levels=c("#bababa", "#000000"))
 
 # Rename adjustment groups
 levels(main_estimates$model) <- list("Age/sex/region adjustment"="mdl_agesex", "Extensive adjustment"="mdl_max_adj")
@@ -113,7 +123,7 @@ for(c in cohort){
 
   ggplot2::ggplot(data=df,
                 mapping = ggplot2::aes(x=time, y = estimate, color = model, shape=model, fill=model))+
-    ggplot2::geom_point(position = ggplot2::position_dodge(width = 0.5)) +
+    ggplot2::geom_point(position = ggplot2::position_dodge(width = 1)) +
     ggplot2::geom_hline(mapping = ggplot2::aes(yintercept = 1), colour = "#A9A9A9") +
     ggplot2::geom_errorbar(mapping = ggplot2::aes(ymin = ifelse(conf.low<0.25,0.25,conf.low), 
                                                   ymax = ifelse(conf.high>64,64,conf.high),  
@@ -125,7 +135,7 @@ for(c in cohort){
     ggplot2::scale_x_continuous(lim = c(0,28), breaks = seq(0,28,4)) +
     ggplot2::scale_fill_manual(values = levels(df$colour), labels = levels(df$model))+ 
     ggplot2::scale_color_manual(values = levels(df$colour), labels = levels(df$model)) +
-    ggplot2::scale_shape_manual(values = c(rep(21,18)), labels = levels(df$model)) +
+    ggplot2::scale_shape_manual(values = c(rep(21,22)), labels = levels(df$model)) +
     ggplot2::labs(x = "\nWeeks since COVID-19 diagnosis", y = "Hazard ratio and 95% confidence interval") +
     ggplot2::guides(fill=ggplot2::guide_legend(ncol = 2, byrow = TRUE)) +
     ggplot2::theme_minimal() +
