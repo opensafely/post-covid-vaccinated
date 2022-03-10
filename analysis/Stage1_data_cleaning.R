fb4f5abbfc785b973bba1e75fe5fe6407cfae8a2
Kurt Taylor <kurt.taylor@bristol.ac.uk>
2022-03-10 08:06:10 +0000

    Update Stage1_data_cleaning.R

--- analysis/Stage1_data_cleaning.R
+++ analysis/Stage1_data_cleaning.R
@@ -142,15 +142,15 @@ stage1 <- function(cohort_name){
     
     #Rule 5: Pregnancy/birth codes for men
     input$rule5=NA
-    input$rule5=(input$qa_bin_pregnancy == TRUE & input$cov_cat_sex=="M")
+    input$rule5=(input$qa_bin_pregnancy == TRUE & input$cov_cat_sex=="Male")
     
     #Rule 6: HRT or COCP meds for men
     input$rule6=NA
-    input$rule6=((input$cov_cat_sex=="M" & input$cov_bin_hormone_replacement_therapy==TRUE)|(input$cov_cat_sex=="M" & input$cov_bin_combined_oral_contraceptive_pill==TRUE))
+    input$rule6=((input$cov_cat_sex=="Male" & input$cov_bin_hormone_replacement_therapy==TRUE)|(input$cov_cat_sex=="Male" & input$cov_bin_combined_oral_contraceptive_pill==TRUE))
     
     #Rule 7: Prostate cancer codes for women
     input$rule7=NA
-    input$rule7=(input$qa_bin_prostate_cancer == TRUE & input$cov_cat_sex=="F")
+    input$rule7=(input$qa_bin_prostate_cancer == TRUE & input$cov_cat_sex=="Female")
     
     
     
