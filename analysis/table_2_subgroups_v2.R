660f997240427118f8a7209591a45efde22fcc04
Yinghui Wei <91749253+yinghuiwei@users.noreply.github.com>
2022-03-23 16:15:19 +0000

    update with population

--- analysis/table_2_subgroups_v2.R
+++ analysis/table_2_subgroups_v2.R
@@ -24,7 +24,7 @@ args <- commandArgs(trailingOnly=TRUE)
 if(length(args)==0){
   # use for interactive testing
   population <- "vaccinated"
-  #population = "electively_unvaccinated"
+  population = "electively_unvaccinated"
 }else{
   population <- args[[1]]
 }
@@ -124,177 +124,187 @@ table_2_calculation <- function(survival_data, event,cohort,subgrp, subgrp_level
 }
 
 
-# define analyses of interests
-
-active_analyses <- read_rds("lib/active_analyses.rds")
-active_analyses <- active_analyses %>%dplyr::filter(active == "TRUE")
-
-analyses_of_interest <- as.data.frame(matrix(ncol = 8,nrow = 0))
-
-outcomes<-active_analyses$outcome_variable
-
-# for testing: i="out_date_ate"
-for(i in outcomes){
-  analyses_to_run <- active_analyses %>% filter(outcome_variable==i)
-  
-  ##Set which cohorts are required
-  
-  if(analyses_to_run$cohort=="all"){
-    cohort_to_run=c("vaccinated", "electively_unvaccinated")
-  }else{
-    analyses_to_run=active_analyses$cohort
-  }  
-  
-  ## Transpose active_analyses to single column so can filter to analysis models to run
-  
-  analyses_to_run <- as.data.frame(t(analyses_to_run))
-  analyses_to_run$subgroup <- row.names(analyses_to_run)
-  colnames(analyses_to_run) <- c("run","subgroup")
- # analyses_to_run$prior_history_var <- analyses_to_run$run
-  #analyses_to_run$prior_history_var <- ifelse(startsWith(row.names(analyses_to_run),"prior_history"),analyses_to_run$run,FALSE)
-  
-  analyses_to_run<- analyses_to_run %>% filter(run=="TRUE"  & subgroup != "active") 
-  rownames(analyses_to_run) <- NULL
-  analyses_to_run <- analyses_to_run %>% select(!run)
-  analyses_to_run$event=i
-  
-  ## Add in  all possible combinations of the subgroups, models and cohorts
-  analyses_to_run <- crossing(analyses_to_run,cohort_to_run)
-  
-  ## Add in which covariates to stratify by
-  analyses_to_run$stratify_by_subgroup=NA
-  for(j in c("ethnicity","sex")){
-    analyses_to_run$stratify_by_subgroup <- ifelse(startsWith(analyses_to_run$subgroup,i),i,analyses_to_run$stratify_by_subgroup)
-  }
-  index = which(active_analyses$outcome_variable == i)
-  analyses_to_run$stratify_by_subgroup <- ifelse(startsWith(analyses_to_run$subgroup,"prior_history"),active_analyses$prior_history_var[index],analyses_to_run$stratify_by_subgroup)
-  analyses_to_run$stratify_by_subgroup <- ifelse(is.na(analyses_to_run$stratify_by_subgroup),analyses_to_run$subgroup,analyses_to_run$stratify_by_subgroup)
-  
-  
-  ## Add in relevant subgroup levels to specify which stratum to run for
-  analyses_to_run$strata <- NA
-  analyses_to_run$strata <- ifelse(analyses_to_run$subgroup=="main","main",analyses_to_run$strata)
-  analyses_to_run$strata <- ifelse(analyses_to_run$subgroup=="covid_history","TRUE",analyses_to_run$strata)
-  
-  for(i in c("covid_pheno_","agegp_","sex_","ethnicity_","prior_history_")){
-    analyses_to_run$strata <- ifelse(startsWith(analyses_to_run$subgroup,i),gsub(i,"",analyses_to_run$subgroup),analyses_to_run$strata)
-  }
-  analyses_of_interest <- rbind(analyses_of_interest,analyses_to_run)
-  
+table_2_subgroups_output <- function(population){
+    # define analyses of interests
+    active_analyses <- read_rds("lib/active_analyses.rds")
+    active_analyses <- active_analyses %>%dplyr::filter(active == "TRUE")
+    
+    analyses_of_interest <- as.data.frame(matrix(ncol = 8,nrow = 0))
+    
+    outcomes<-active_analyses$outcome_variable
+    
+    # for testing: i="out_date_ate"
+    for(i in outcomes){
+      analyses_to_run <- active_analyses %>% filter(outcome_variable==i)
+      
+      ##Set which cohorts are required
+      
+      if(analyses_to_run$cohort=="all"){
+        cohort_to_run=c("vaccinated", "electively_unvaccinated")
+      }else{
+        analyses_to_run=active_analyses$cohort
+      }  
+      
+      ## Transpose active_analyses to single column so can filter to analysis models to run
+      
+      analyses_to_run <- as.data.frame(t(analyses_to_run))
+      analyses_to_run$subgroup <- row.names(analyses_to_run)
+      colnames(analyses_to_run) <- c("run","subgroup")
+     # analyses_to_run$prior_history_var <- analyses_to_run$run
+      #analyses_to_run$prior_history_var <- ifelse(startsWith(row.names(analyses_to_run),"prior_history"),analyses_to_run$run,FALSE)
+      
+      analyses_to_run<- analyses_to_run %>% filter(run=="TRUE"  & subgroup != "active") 
+      rownames(analyses_to_run) <- NULL
+      analyses_to_run <- analyses_to_run %>% select(!run)
+      analyses_to_run$event=i
+      
+      ## Add in  all possible combinations of the subgroups, models and cohorts
+      analyses_to_run <- crossing(analyses_to_run,cohort_to_run)
+      
+      ## Add in which covariates to stratify by
+      analyses_to_run$stratify_by_subgroup=NA
+      for(j in c("ethnicity","sex")){
+        analyses_to_run$stratify_by_subgroup <- ifelse(startsWith(analyses_to_run$subgroup,i),i,analyses_to_run$stratify_by_subgroup)
+      }
+      index = which(active_analyses$outcome_variable == i)
+      analyses_to_run$stratify_by_subgroup <- ifelse(startsWith(analyses_to_run$subgroup,"prior_history"),active_analyses$prior_history_var[index],analyses_to_run$stratify_by_subgroup)
+      analyses_to_run$stratify_by_subgroup <- ifelse(is.na(analyses_to_run$stratify_by_subgroup),analyses_to_run$subgroup,analyses_to_run$stratify_by_subgroup)
+      
+      
+      ## Add in relevant subgroup levels to specify which stratum to run for
+      analyses_to_run$strata <- NA
+      analyses_to_run$strata <- ifelse(analyses_to_run$subgroup=="main","main",analyses_to_run$strata)
+      analyses_to_run$strata <- ifelse(analyses_to_run$subgroup=="covid_history","TRUE",analyses_to_run$strata)
+      
+      for(i in c("covid_pheno_","agegp_","sex_","ethnicity_","prior_history_")){
+        analyses_to_run$strata <- ifelse(startsWith(analyses_to_run$subgroup,i),gsub(i,"",analyses_to_run$subgroup),analyses_to_run$strata)
+      }
+      analyses_of_interest <- rbind(analyses_of_interest,analyses_to_run)
+      
+    }
+    
+    analyses_of_interest <- analyses_of_interest %>% filter(cohort_to_run == population)
+    write.csv(analyses_of_interest, file=paste0("output/analyses_of_interest", population, ".csv"))
+    #ir = incidence rate; ir_lower = lower bound of the 95% CI for ir; ir_upper = upper bound of the 95% CI for ir
+    unexposed_person_days <- unexposed_event_count <- unexposed_ir <- unexposed_ir_lower <- unexposed_ir_upper <- rep("NA", nrow(analyses_of_interest))
+    exposed_person_days <- exposed_event_count <- exposed_ir <- exposed_ir_lower <- exposed_ir_upper <- rep("NA", nrow(analyses_of_interest))
+    
+    analyses_of_interest <- cbind(analyses_of_interest, unexposed_person_days, unexposed_event_count, unexposed_ir, unexposed_ir_lower, unexposed_ir_upper,
+                                       exposed_person_days, exposed_event_count, exposed_ir, exposed_ir_lower, exposed_ir_upper)
+    
+    # # specify subgroup names
+    index <- grepl("agegp", analyses_of_interest$subgroup, fixed = TRUE)
+    analyses_of_interest$stratify_by_subgroup[index] <- "sub_cat_age_group"
+    
+    index <- grepl("covid_history", analyses_of_interest$subgroup, fixed = TRUE)
+    analyses_of_interest$stratify_by_subgroup[index] <- "sub_bin_covid19_confirmed_history"
+    
+    index <- grepl("covid_pheno_", analyses_of_interest$subgroup, fixed = TRUE)
+    analyses_of_interest$stratify_by_subgroup[index] <- "sub_cat_covid19_hospital"
+    
+    index <- grepl("ethnicity", analyses_of_interest$subgroup, fixed = TRUE)
+    analyses_of_interest$stratify_by_subgroup[index] <- "sub_cat_ethnicity"
+    
+    index <- grepl("sex", analyses_of_interest$subgroup, fixed = TRUE)
+    analyses_of_interest$stratify_by_subgroup[index] <- "sub_bin_sex"
+    
+    index <- grepl("main", analyses_of_interest$subgroup, fixed = TRUE)
+    analyses_of_interest$stratify_by_subgroup[index] <- "sub_main"
+    
+    index <- grepl("ate", analyses_of_interest$subgroup, fixed = TRUE)
+    analyses_of_interest$stratify_by_subgroup[index] <- "sub_bin_ate"
+    
+    index <- grepl("cov_bin_vte", analyses_of_interest$stratify_by_subgroup, fixed = TRUE)
+    analyses_of_interest$stratify_by_subgroup[index] <- "sub_bin_vte"
+    
+    # read in data------------------------------------------------------------
+    
+    input <- read_rds(paste0("output/input_",population,"_stage1.rds"))
+    
+    # Define age groups
+    input$cov_cat_age_group <- ""
+    input$cov_cat_age_group <- ifelse(input$cov_num_age>=18 & input$cov_num_age<=39, "18_39", input$cov_cat_age_group)
+    input$cov_cat_age_group <- ifelse(input$cov_num_age>=40 & input$cov_num_age<=59, "40_59", input$cov_cat_age_group)
+    input$cov_cat_age_group <- ifelse(input$cov_num_age>=60 & input$cov_num_age<=79, "60_79", input$cov_cat_age_group)
+    input$cov_cat_age_group <- ifelse(input$cov_num_age>=80, "80_110", input$cov_cat_age_group)
+    
+    # rename variables to indicate them as subgroups
+    setnames(input,
+           old = c("cov_cat_sex",
+                   "cov_cat_age_group",
+                   "cov_cat_ethnicity",
+                   "cov_bin_vte"),
+           new = c("sub_bin_sex",
+                   "sub_cat_age_group",
+                   "sub_cat_ethnicity",
+                   "sub_bin_vte"))
+    
+    levels(input$sub_cat_ethnicity) <- c("White", "Mixed", "South_Asian", "Black", "Other", "Missing")
+    event_dates_names <- active_analyses$outcome_variable
+    outcome_names <- tidyselect::vars_select(names(input), starts_with(c("out_"), ignore.case=TRUE))
+    outcome_names_not_active <- outcome_names[!outcome_names %in% event_dates_names]
+    
+    input$sub_main <- "main"
+    
+    sub_grp_names <- tidyselect::vars_select(names(input), starts_with(c('sub_'), ignore.case = TRUE))
+    
+    vars_names <- tidyselect::vars_select(names(input), !starts_with(c('cov_','qa_','vax_cat'), ignore.case = TRUE))
+    vars_names <- vars_names[!vars_names %in% outcome_names_not_active]
+    
+    survival_data <- input[,vars_names]
+    
+    rm(list=c("input"))
+    
+    survival_data <- survival_data %>% mutate(cohort_start_date = cohort_start,cohort_end_date = cohort_end)
+    
+    # rewrite the function
+    # testing
+    #event="out_date_ami";cohort="vaccinated";strata="covid_history"; subgrp_level="TRUE"; subgrp_full_name="sub_bin_covid19_confirmed_history"
+    #event="vte";cohort="vaccinated";strata="sub_bin_vte"; subgrp_level="FALSE"; subgrp_full_name="sub_bin_vte"
+    
+    col_names <- names(analyses_of_interest)
+    start = grep("unexposed_person_days", col_names)
+    end = ncol(analyses_of_interest)
+    
+    for(i in 1:nrow(analyses_of_interest)){
+      #for quick testing
+      #for(i in 357:358){
+      #for(i in 87:88){
+      #for(i in 5:6){
+      d <- analyses_of_interest
+      print(i)
+      analyses_of_interest[i,start:end] <- table_2_calculation(survival_data, 
+                                                               event=analyses_of_interest$event[i],
+                                                               cohort=analyses_of_interest$cohort_to_run[i],
+                                                               subgrp=analyses_of_interest$subgroup[i], 
+                                                               subgrp_level=analyses_of_interest$strata[i], 
+                                                               subgrp_full_name=analyses_of_interest$stratify_by_subgroup[i])
+    }
+    
+    # results <- lapply(split(analyses_of_interest,seq(nrow(analyses_of_interest))),
+    #        function(analyses_of_interest)
+    #          table_2_calculation(
+    #            survival_data = survival_data,
+    #            event=analyses_of_interest$event,
+    #            cohort=analyses_of_interest$cohort_to_run,
+    #            subgrp=analyses_of_interest$subgroup,
+    #            subgrp_level=analyses_of_interest$strata,
+    #            subgrp_full_name=analyses_of_interest$stratify_by_subgroup)
+    # )
+    
+    write.csv(analyses_of_interest, file=paste0("output/table_2_subgroups", population, ".csv"), row.names = F)
+    input1_aer <- analyses_of_interest %>% select(c("event", "cohort_to_run", "subgroup", "strata", "unexposed_person_days"))
+    names(input1_aer)[which(names(input1_aer) == "cohort_to_run")] = "cohort"
+    input1_aer$event <- ifelse(startsWith(input1_aer$event,"out_"),gsub("out_date_","",input1_aer$event),input1_aer$evevent)
+    write.csv(input1_aer, file=paste0("output/input1_aer", population, ".csv"), row.names=F)
 }
 
-#ir = incidence rate; ir_lower = lower bound of the 95% CI for ir; ir_upper = upper bound of the 95% CI for ir
-unexposed_person_days <- unexposed_event_count <- unexposed_ir <- unexposed_ir_lower <- unexposed_ir_upper <- rep("NA", nrow(analyses_of_interest))
-exposed_person_days <- exposed_event_count <- exposed_ir <- exposed_ir_lower <- exposed_ir_upper <- rep("NA", nrow(analyses_of_interest))
+# Run function using specified commandArgs
 
-analyses_of_interest <- cbind(analyses_of_interest, unexposed_person_days, unexposed_event_count, unexposed_ir, unexposed_ir_lower, unexposed_ir_upper,
-                                   exposed_person_days, exposed_event_count, exposed_ir, exposed_ir_lower, exposed_ir_upper)
-
-# # specify subgroup names
-index <- grepl("agegp", analyses_of_interest$subgroup, fixed = TRUE)
-analyses_of_interest$stratify_by_subgroup[index] <- "sub_cat_age_group"
-
-index <- grepl("covid_history", analyses_of_interest$subgroup, fixed = TRUE)
-analyses_of_interest$stratify_by_subgroup[index] <- "sub_bin_covid19_confirmed_history"
-
-index <- grepl("covid_pheno_", analyses_of_interest$subgroup, fixed = TRUE)
-analyses_of_interest$stratify_by_subgroup[index] <- "sub_cat_covid19_hospital"
-
-index <- grepl("ethnicity", analyses_of_interest$subgroup, fixed = TRUE)
-analyses_of_interest$stratify_by_subgroup[index] <- "sub_cat_ethnicity"
-
-index <- grepl("sex", analyses_of_interest$subgroup, fixed = TRUE)
-analyses_of_interest$stratify_by_subgroup[index] <- "sub_bin_sex"
-
-index <- grepl("main", analyses_of_interest$subgroup, fixed = TRUE)
-analyses_of_interest$stratify_by_subgroup[index] <- "sub_main"
-
-index <- grepl("ate", analyses_of_interest$subgroup, fixed = TRUE)
-analyses_of_interest$stratify_by_subgroup[index] <- "sub_bin_ate"
-
-index <- grepl("cov_bin_vte", analyses_of_interest$stratify_by_subgroup, fixed = TRUE)
-analyses_of_interest$stratify_by_subgroup[index] <- "sub_bin_vte"
-
-# read in data------------------------------------------------------------
-
-input <- read_rds(paste0("output/input_",population,"_stage1.rds"))
-
-# Define age groups
-input$cov_cat_age_group <- ""
-input$cov_cat_age_group <- ifelse(input$cov_num_age>=18 & input$cov_num_age<=39, "18_39", input$cov_cat_age_group)
-input$cov_cat_age_group <- ifelse(input$cov_num_age>=40 & input$cov_num_age<=59, "40_59", input$cov_cat_age_group)
-input$cov_cat_age_group <- ifelse(input$cov_num_age>=60 & input$cov_num_age<=79, "60_79", input$cov_cat_age_group)
-input$cov_cat_age_group <- ifelse(input$cov_num_age>=80, "80_110", input$cov_cat_age_group)
-
-# rename variables to indicate them as subgroups
-setnames(input,
-       old = c("cov_cat_sex",
-               "cov_cat_age_group",
-               "cov_cat_ethnicity",
-               "cov_bin_vte"),
-       new = c("sub_bin_sex",
-               "sub_cat_age_group",
-               "sub_cat_ethnicity",
-               "sub_bin_vte"))
-
-levels(input$sub_cat_ethnicity) <- c("White", "Mixed", "South_Asian", "Black", "Other", "Missing")
-event_dates_names <- active_analyses$outcome_variable
-outcome_names <- tidyselect::vars_select(names(input), starts_with(c("out_"), ignore.case=TRUE))
-outcome_names_not_active <- outcome_names[!outcome_names %in% event_dates_names]
-
-
-input$sub_main <- "main"
-
-sub_grp_names <- tidyselect::vars_select(names(input), starts_with(c('sub_'), ignore.case = TRUE))
-
-vars_names <- tidyselect::vars_select(names(input), !starts_with(c('cov_','qa_','vax_cat'), ignore.case = TRUE))
-vars_names <- vars_names[!vars_names %in% outcome_names_not_active]
-
-survival_data <- input[,vars_names]
-
-rm(list=c("input"))
-
-survival_data <- survival_data %>% mutate(cohort_start_date = cohort_start,cohort_end_date = cohort_end)
-
-# rewrite the function
-# testing
-#event="out_date_ami";cohort="vaccinated";strata="covid_history"; subgrp_level="TRUE"; subgrp_full_name="sub_bin_covid19_confirmed_history"
-#event="vte";cohort="vaccinated";strata="sub_bin_vte"; subgrp_level="FALSE"; subgrp_full_name="sub_bin_vte"
-
-col_names <- names(analyses_of_interest)
-start = grep("unexposed_person_days", col_names)
-end = ncol(analyses_of_interest)
-
-for(i in 1:nrow(analyses_of_interest)){
-  #for quick testing
-  #for(i in 357:358){
-  #for(i in 87:88){
-  #for(i in 5:6){
-  d <- analyses_of_interest
-  print(i)
-  analyses_of_interest[i,start:end] <- table_2_calculation(survival_data, 
-                                                           event=analyses_of_interest$event[i],
-                                                           cohort=analyses_of_interest$cohort_to_run[i],
-                                                           subgrp=analyses_of_interest$subgroup[i], 
-                                                           subgrp_level=analyses_of_interest$strata[i], 
-                                                           subgrp_full_name=analyses_of_interest$stratify_by_subgroup[i])
+if(population == "both"){
+  table_2_subgroups_output("vaccinated")
+  table_2_subgroups_output("electively_unvaccinated")
+}else{
+  table_2_subgroups_output(population)
 }
-
-# results <- lapply(split(analyses_of_interest,seq(nrow(analyses_of_interest))),
-#        function(analyses_of_interest)
-#          table_2_calculation(
-#            survival_data = survival_data,
-#            event=analyses_of_interest$event,
-#            cohort=analyses_of_interest$cohort_to_run,
-#            subgrp=analyses_of_interest$subgroup,
-#            subgrp_level=analyses_of_interest$strata,
-#            subgrp_full_name=analyses_of_interest$stratify_by_subgroup)
-# )
-
-write.csv(analyses_of_interest, file=paste0("output/table_2_subgroups", ".csv"), row.names = F)
-input1_aer <- analyses_of_interest %>% select(c("event", "cohort_to_run", "stratify_by_subgroup", "subgroup", "strata", "unexposed_person_days"))
-write.csv(input1_aer, file=paste0("output/input1_aer", ".csv"), row.names=F)
-
-
-
 
