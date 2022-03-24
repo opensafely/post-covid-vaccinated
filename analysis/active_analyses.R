541dfd3620ceb1922719fcd5201532585d502bfe
Venexia Walker <venexia.walker@bristol.ac.uk>
2022-03-24 16:58:57 +0000

    Revert "Add medical history covariates"
    This reverts commit d3971cacdd62d9c6b1a67bc6c5e9699e4443012a.

--- analysis/active_analyses.R
+++ analysis/active_analyses.R
@@ -118,7 +118,7 @@ for (i in 1:length(outcomes)) {
   df[nrow(df)+1,] <- c(TRUE,
                        outcomes[i],
                        paste0("out_date_",outcomes_short[i]),
-                       "cov_num_consulation_rate;cov_bin_healthcare_worker;cov_num_age;cov_cat_ethnicity;cov_cat_deprivation;cov_cat_region;cov_cat_smoking_status;cov_bin_carehome_status;cov_cat_sex;cov_bin_lipid_medications;cov_bin_dementia;cov_bin_liver_disease;cov_bin_chronic_kidney_disease;cov_bin_cancer;cov_bin_hypertension;cov_bin_diabetes;cov_bin_obesity;cov_bin_chronic_obstructive_pulmonary_disease;cov_bin_depression;cov_bin_anxiety;cov_bin_eating_disorders;cov_bin_serious_mental_illness;cov_bin_self_harm",
+                       "cov_num_consulation_rate;cov_bin_healthcare_worker;cov_num_age;cov_cat_ethnicity;cov_cat_deprivation;cov_cat_region;cov_cat_smoking_status;cov_bin_carehome_status;cov_cat_sex",
                        rep("all",2),
                        rep(TRUE,4),
                        rep(FALSE,14),
