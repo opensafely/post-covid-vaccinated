7ef2b51122f561d67e5fdb1ab0694aa2d850afb8
RochelleKnight <rochelle.knight@bristol.ac.uk>
2022-04-04 15:55:57 +0100

    Update format_tbls_HRs.R

--- analysis/format_tbls_HRs.R
+++ analysis/format_tbls_HRs.R
@@ -216,7 +216,8 @@ if(length(results_done)>0){
   write.csv(supressed_combined_hr_event_counts,paste0(output_dir,"/suppressed_compiled_HR_results_",event_name,"_", cohort,".csv") , row.names=F)
   print(paste0("Supressed HR with event counts saved: ", output_dir,"/suppressed_compiled_HR_results_",event_name,"_", cohort,".csv"))
   
-  supressed_combined_hr_event_counts <- supressed_combined_hr_event_counts %>% select(!c("expo_week","events_total"))
+  
+  supressed_combined_hr_event_counts <- supressed_combined_hr_event_counts[!colnames(supressed_combined_hr_event_counts) %in% c("expo_week","events_total")]
   write.csv(supressed_combined_hr_event_counts,paste0(output_dir,"/suppressed_compiled_HR_results_",event_name,"_", cohort,"to_release.csv") , row.names=F)
   
 }
