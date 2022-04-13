c05cdf7987d8d6edf407aab5fb6dbfce119a81e2
Yinghui Wei <91749253+yinghuiwei@users.noreply.github.com>
2022-04-13 12:22:18 +0100

    sort out yaml actions

--- analysis/create_project_actions.R
+++ analysis/create_project_actions.R
@@ -140,8 +140,14 @@ apply_table2_new_function <- function(analyses, cohort){
     action(
       name = glue("stage4_table_2_{analyses}_{cohort}"),
       run = "r:latest analysis/table_2_new.R",
-      needs = list("stage4_input_for_table_2_vaccinated", 
-                   "stage4_input_for_table_2_electively_unvaccinated"),
+      if(cohort == "vaccinated"){
+        needs = list("stage4_input_for_table_2_vaccinated")
+      },
+      if(cohort == "electively_unvaccinated"){
+        needs = list("stage4_input_for_table_2_electively_unvaccinated")
+      },
+      # needs = list("stage4_input_for_table_2_vaccinated", 
+      #              "stage4_input_for_table_2_electively_unvaccinated"),
       arguments = c(analyses, cohort),
       moderately_sensitive = list(
         table_2_csv = glue("output/table2_{analyses}_{cohort}.csv"),
