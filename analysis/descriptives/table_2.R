1cd0904ae292019adeb54e717c3ae795791e2fff
RochelleKnight <rochelle.knight@bristol.ac.uk>
2022-05-18 16:41:56 +0100

    Revert "correct when to +1 for unexposed person days"
    This reverts commit 96606221bea31f5f8c5a27bb2c6e35e1d3a6093e.

--- analysis/descriptives/table_2.R
+++ analysis/descriptives/table_2.R
@@ -215,6 +215,7 @@ table_2_calculation <- function(survival_data, event,cohort,subgroup, stratify_b
   
   # calculate unexposed follow-up days for AER script
   data_active = data_active %>% mutate(person_days_unexposed = as.numeric((as.Date(follow_up_end_unexposed) - as.Date(index_date))))
+  index <- which(data_active$follow_up_end_unexposed > data_active$exp_date_covid19_confirmed | is.na(data_active$exp_date_covid19_confirmed))
   data_active$person_days_unexposed[index] = data_active$person_days_unexposed[index] + 1
   
   if(subgroup == "covid_pheno_hospitalised"){
