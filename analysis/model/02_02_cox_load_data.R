dee46f02a45b2f6e0312d3bb263ce5ca55bcaeb7
RochelleKnight <rochelle.knight@bristol.ac.uk>
2022-05-27 18:53:46 +0100

    0-7 day time period

--- analysis/model/02_02_cox_load_data.R
+++ analysis/model/02_02_cox_load_data.R
@@ -57,8 +57,8 @@ cohort_end_date <- as.Date("2021-12-14")
 #a reduced number of time periods is used (need 197 instead of 196 as time periods are split using [ , ) 
 
 #cuts_days_since_expo <- c(28, 197) 
-cuts_days_since_expo <- c(3,7, 14, 28, 56, 84, 197) 
-cuts_days_since_expo_reduced <- c(3,28,197) 
+cuts_days_since_expo <- c(7, 14, 28, 56, 84, 197) 
+cuts_days_since_expo_reduced <- c(7,28,197) 
 
 #Rename input variable names (by renaming here it means that these scripts can be used for other datasets without
 ## having to keep updating all the variable names throughout the following scripts)
