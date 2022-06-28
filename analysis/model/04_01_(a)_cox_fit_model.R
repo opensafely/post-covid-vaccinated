6eaedc7fd579de0e6195e8eb2c47212e17e9c7ab
RochelleKnight <rochelle.knight@bristol.ac.uk>
2022-06-28 09:51:12 +0100

    Revert "Merge pull request #229 from opensafely/test-run-age-sex-region-covar"
    This reverts commit b4ae4355c93562b0f7130bf1a569082e952f578c, reversing
    changes made to 324783a6ecedfa6fd705b058a62857245431fefb.

--- analysis/model/04_01_(a)_cox_fit_model.R
+++ analysis/model/04_01_(a)_cox_fit_model.R
@@ -134,14 +134,10 @@ coxfit <- function(data_surv, interval_names, covar_names, subgroup, mdl){
   colnames(combined_results) <- c("term","estimate","conf.low","conf.high","std.error","robust.se","covariate","P","mdl")
   
   #for(test_model in c("no_region_mdl_max_adj","region_covar_mdl_max_adj","region_strata_mdl_max_adj","region_strata_option_1_mdl_max_adj","region_strata_option_2_mdl_max_adj")){
-  for(test_model in c("age_sex_region_covar", "age_sex_ethnicity_region_covar","age_sex_ethnicity_deprivation_region_covar" )){
+  for(test_model in c("region_covar_mdl_max_adj","region_strata_mdl_max_adj")){
       
-    if(test_model == "age_sex_region_covar"){
-      model = "mdl_agesex"
-    }else{
-      model="mdl_max_adj"
-    }
-   
+    model="mdl_max_adj"
+    
     
     #Base formula
     if(test_model=="no_region_mdl_max_adj"){
@@ -215,21 +211,6 @@ coxfit <- function(data_surv, interval_names, covar_names, subgroup, mdl){
         "Surv(tstart, tstop, event) ~ ",
         paste(covariates_excl_region_sex_age, collapse="+"), 
         "+ cluster(patient_id) + region_name")
-    }else if(test_model =="age_sex_region_covar"){
-      surv_formula <- paste0(
-             "Surv(tstart, tstop, event) ~ ",
-             paste(interval_names, collapse="+"),
-             "+ cluster(patient_id) + region_name")
-    }else if(test_model == "age_sex_ethnicity_region_covar"){
-      surv_formula <- paste0(
-        "Surv(tstart, tstop, event) ~ ",
-        paste(interval_names, collapse="+"),
-        "+ cluster(patient_id) + region_name")
-    }else if(test_model == "age_sex_ethnicity_deprivation_region_covar"){
-      surv_formula <- paste0(
-        "Surv(tstart, tstop, event) ~ ",
-        paste(interval_names, collapse="+"),
-        "+ cluster(patient_id) + region_name + cov_cat_deprivation")
     }
     
     
