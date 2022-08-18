f045d19700626a017dd367705ee22b92d629dc2e
RochelleKnight <rochelle.knight@bristol.ac.uk>
2022-08-18 12:14:39 +0100

    don't collapse time periods

--- analysis/model/04_01_(b)_cox_format_survival_data.R
+++ analysis/model/04_01_(b)_cox_format_survival_data.R
@@ -316,11 +316,11 @@ fit_get_data_surv <- function(event,subgroup, stratify_by_subgroup, stratify_by,
     print(episode_info)
     
     #Any time periods with <=5 events? If yes, will reduce time periods
-    ind_any_zeroeventperiod <- any((episode_info$events_total <= 5) & (!identical(cuts_days_since_expo, c(28, 197))))
+    #ind_any_zeroeventperiod <- any((episode_info$events_total <= 5) & (!identical(cuts_days_since_expo, c(28, 197))))
     
-    if(time_point == "alternative"){
-      ind_any_zeroeventperiod = "FALSE"
-    }
+    
+    ind_any_zeroeventperiod = "FALSE"
+    
     
     #Are there <50 post expo events? If yes, won't run analysis
     #Can change <50 to be lower to test on dummy data
