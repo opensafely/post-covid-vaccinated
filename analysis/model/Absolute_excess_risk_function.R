fb45f93c0786089884f8ff8517628a88d7f4c908
RochelleKnight <rochelle.knight@bristol.ac.uk>
2022-09-22 13:33:52 +0100

    AER updates

--- analysis/model/Absolute_excess_risk_function.R
+++ analysis/model/Absolute_excess_risk_function.R
@@ -21,21 +21,23 @@
 ## =============================================================================
 
 # # CODE FOR SINGLE AER
-# event_of_interest="ate"
-# cohort_of_interest="vaccinated"
-# model_of_interest="mdl_max_adj"     #Should always be maximum adjusted model
+#event_of_interest="ate"
+#cohort_of_interest="vaccinated"
+#model_of_interest="mdl_max_adj"     #Should always be maximum adjusted model
+#subgroup_of_interest="aer_Female_40_59"
 
-excess_risk <- function(event_of_interest, cohort_of_interest, model_of_interest, input) {
+excess_risk <- function(event_of_interest, cohort_of_interest, model_of_interest,subgroup_of_interest, input) {
   
   
   #-------------------------Check structure the input---------------------------
-  input <- input %>% mutate(across(c("unexposed_person_days","unexposed_event_count","total_covid19_cases"), as.numeric))
+  input <- input %>% mutate(across(c("estimate_main","estimate_subgroup","unexposed_person_days","unexposed_event_count","total_covid19_cases"), as.numeric))
   input <- as.data.frame(input)
   
   #---------------------------------Subset to relevant data---------------------
-  input <- input[input$event == event_of_interest & 
-                   input$model == model_of_interest  & 
-                   input$cohort == cohort_of_interest,]
+  input <- input[input$event == event_of_interest
+                 & input$model == model_of_interest
+                 & input$cohort == cohort_of_interest
+                 & input$subgroup == subgroup_of_interest,]
   
   #----Add start and end days for time periods which are needed for lifetable---
   for(i in c("time_period_start","time_period_end")){
@@ -49,67 +51,75 @@ excess_risk <- function(event_of_interest, cohort_of_interest, model_of_interest
   
   
   #---------------------------------------------------------------
-  # LIFETABLE APPROACH - loop through each age/sex subgroup
+  # LIFETABLE APPROACH
   #---------------------------------------------------------------
-  lifetable <- data.frame(c(0:max(input$time_period_end)))
+  lifetable <- data.frame(c(0:(max(input$time_period_end)-1)))
   
   colnames(lifetable) <- c("days")
   lifetable$event <- event_of_interest
   lifetable$cohort <- cohort_of_interest
   lifetable$model <- model_of_interest
-
-  for(l in c("Female","Male")){
-    for(m in agelabels){
-      
-      #Step1. Calculate average daily incidence of outcome in unexposed age/sex subgroups
-      #As number of events (in the unexposed) divided by time (days)
-      lifetable[,paste0(l,"_",m,"_incidence_unexp")] <- input$unexposed_event_count[min(which(input$subgroup == paste0("aer_",l,"_",m)))] / input$unexposed_person_days[min(which(input$subgroup == paste0("aer_",l,"_",m)))]
-      
-      #Step2. Use life table approach to calculate cumulative risk over time in unexposed age/sex subgroups
-      lifetable[,paste0(l,"_",m,"_cumulative_survival_unexp")] <- cumprod(1 - lifetable[,paste0(l,"_",m,"_incidence_unexp")]) 
-      
-      #Step3. Carry over maximally adjusted hazard ratio estimates (time varying) for event after COVID exposure
-      lifetable[,paste0(l,"_",m,"_hr")] <- NA
-      tmp1 <- input %>%filter(subgroup == paste0("aer_",l,"_",m))
-      for(i in 1:nrow(tmp1)){
-        tmp2 <- tmp1[i,]
-        lifetable[,paste0(l,"_",m,"_hr")] <- ifelse(lifetable$days >= tmp2$time_period_start & lifetable$days <= tmp2$time_period_end, tmp2$estimate, lifetable[,paste0(l,"_",m,"_hr")])
-      }
-      
-      #Occasionally HR is redacted, is so set to null
-      lifetable[,paste0(l,"_",m,"_hr")] <- ifelse(lifetable[,paste0(l,"_",m,"_hr")]=="[Redacted]",NA,lifetable[,paste0(l,"_",m,"_hr")])
-      lifetable[,paste0(l,"_",m,"_hr")] <- as.numeric(lifetable[,paste0(l,"_",m,"_hr")])
-
-      #Step4. Calculate predicted incidence of outcome after COVID exposure in age/sex subgroups
-      #Multiply daily incidence in unexposed by maximally adjusted hazard ratios (time varying) for event after COVID exposure
-      lifetable[,paste0(l,"_",m,"_cumulative_survival_exp")] <- cumprod(1 - (lifetable[,paste0(l,"_",m,"_hr")] * lifetable[,paste0(l,"_",m,"_incidence_unexp")])) 
-      
-      #Step5. Calculate daily excess risk
-      #As difference in cumulative survival unexposed and expected cumulative survival in unexposed
-      lifetable[,paste0(l,"_",m,"_excess_risk")] <- lifetable[,paste0(l,"_",m,"_cumulative_survival_exp")] -lifetable[,paste0(l,"_",m,"_cumulative_survival_unexp")]
-      
-      #Step6. Carry over total number of COVID cases in age/sex subgroup, calculate subgroup-specific absolute excess risk
-      lifetable[,paste0(l,"_",m,"_total_covid19_cases")] <- input$total_covid19_cases[min(which(input$subgroup == paste0("aer_",l,"_",m)))]
-      lifetable[,paste0(l,"_",m,"_AER")] <- lifetable[,paste0(l,"_",m,"_excess_risk")] * lifetable[,paste0(l,"_",m,"_total_covid19_cases")] 
-    }
+  lifetable$subgroup <- subgroup_of_interest
+  
+  
+  #Step1. Calculate average daily incidence of outcome in unexposed age/sex subgroups
+  #As number of events (in the unexposed) divided by time (days)
+  lifetable$incidence_unexp <- input$unexposed_event_count[1] / input$unexposed_person_days[1]
+  
+  #Step2. Use life table approach to calculate cumulative risk over time in unexposed age/sex subgroups
+  lifetable$cumulative_survival_unexp <- cumprod(1 - lifetable$incidence_unexp) 
+  
+  #Step3. Carry over maximally adjusted hazard ratio estimates (time varying) for event after COVID exposure
+  lifetable$hr_main <- NA
+  
+  for(i in 1:nrow(input)){
+    tmp <- input[i,]
+    lifetable$hr_main <- ifelse(lifetable$days >= tmp$time_period_start & lifetable$days < tmp$time_period_end, tmp$estimate_main, lifetable$hr_main)
   }
+  
+  lifetable$hr_subgroup <- NA
+  
+  for(i in 1:nrow(input)){
+    tmp <- input[i,]
+    lifetable$hr_subgroup <- ifelse(lifetable$days >= tmp$time_period_start & lifetable$days < tmp$time_period_end, tmp$estimate_subgroup, lifetable$hr_subgroup)
+  }
+  
+  #Occasionally HR is redacted, is so set to null
+  # These have been removed at an earlier stage
+  
+  #lifetable$hr <- ifelse(lifetable$hr=="[Redacted]",NA,lifetable$hr)
+  #lifetable$hr <- as.numeric(lifetable$hr)
+  
+  #Step4. Calculate predicted incidence of outcome after COVID exposure in age/sex subgroups
+  #Multiply daily incidence in unexposed by maximally adjusted hazard ratios (time varying) for event after COVID exposure
+  lifetable$cumulative_survival_exp_main <- cumprod(1 - (lifetable$hr_main * lifetable$incidence_unexp))
+  lifetable$cumulative_survival_exp_subgroup <- cumprod(1 - (lifetable$hr_subgroup * lifetable$incidence_unexp))
+  
+  #Step5. Calculate daily excess risk
+  #As difference in cumulative survival unexposed and expected cumulative survival in unexposed
+  lifetable$excess_risk_main <- lifetable$cumulative_survival_exp_main -lifetable$cumulative_survival_unexp
+  lifetable$excess_risk_subgroup <- lifetable$cumulative_survival_exp_subgroup -lifetable$cumulative_survival_unexp
+  
+  #Step6. Carry over total number of COVID cases in age/sex subgroup, calculate subgroup-specific absolute excess risk
+  lifetable$total_covid19_cases <- input$total_covid19_cases[1]
+  lifetable$AER_main <- lifetable$excess_risk_main * lifetable$total_covid19_cases
+  lifetable$AER_subgroup <- lifetable$excess_risk_subgroup * lifetable$total_covid19_cases
 
   #Step7. Total AER as total of all subgroups
 
-  lifetable$total_AER = rowSums(select(lifetable, ends_with("_AER")))
+  #lifetable$total_AER = rowSums(select(lifetable, ends_with("_AER")))
   
   # write output for AER figure
-  write.csv(lifetable, file=paste0(aer_raw_results_dir, "/lifetable_" , cohort_of_interest, "_", model_of_interest, "_", event_of_interest,".csv"), row.names = F)
+  write.csv(lifetable, file=paste0(aer_raw_output_dir, "/lifetable_" ,event_of_interest, "_" ,subgroup_of_interest, "_",cohort_of_interest, ".csv"), row.names = F)
 
   
-  AER_followup_end <- lifetable[nrow(lifetable),]$total_AER
+  #AER_followup_end <- lifetable[nrow(lifetable),]$total_AER
   
-  results <- data.frame(event=event_of_interest,
-                        cohort=cohort_of_interest,
-                        model=model_of_interest,
-                        AER_followup_end=AER_followup_end)
+  # results <- data.frame(event=event_of_interest,
+  #                       cohort=cohort_of_interest,
+  #                       model=model_of_interest,
+  #                       AER_followup_end=AER_followup_end)
   
-  write.csv(results, paste0(aer_raw_results_dir, "/AER_raw_results_", cohort_of_interest, "_", model_of_interest, "_", event_of_interest,".csv"), row.names = F)
-  return(results)
-  #return(print(results)) 
+  #write.csv(results, paste0(aer_raw_results_dir, "/AER_raw_results_", cohort_of_interest, "_", model_of_interest, "_", event_of_interest,".csv"), row.names = F)
+  #return(results)
 }
--- analysis/model/Absolute_excess_risk_scripts.R
+++ analysis/model/Absolute_excess_risk_scripts.R
@@ -7,13 +7,21 @@
 # 1. load the right input data and make sure of the file names and variable structure
 # 2. Cntrl+A run the whole script and find the results in working directory
 
-fs::dir_create(here::here("output", "review", "AER_results"))
-fs::dir_create(here::here("output", "not-for-review", "AER_results"))
+results_dir <- "C:/Users/zy21123/OneDrive - University of Bristol/Documents/OpenSAFELY/Outputs/release/"
+aer_raw_output_dir <- "C:/Users/zy21123/OneDrive - University of Bristol/Documents/OpenSAFELY/Outputs/Figures/AER/raw_results/"
+aer_compiled_output_dir <- "C:/Users/zy21123/OneDrive - University of Bristol/Documents/OpenSAFELY/Outputs/Figures/AER/compiled_results/"
 scripts_dir <- "analysis/model"
-hr_dir <- "output/review/model"
-table_2_dir <- "output/review/descriptives"
-aer_raw_results_dir <- "output/not-for-review/AER_results"
-aer_results_dir <- "output/review/AER_results"
+
+dir.create(file.path(aer_raw_output_dir), recursive =TRUE, showWarnings = FALSE)
+dir.create(file.path(aer_compiled_output_dir), recursive =TRUE, showWarnings = FALSE)
+
+# fs::dir_create(here::here("output", "review", "AER_results"))
+# fs::dir_create(here::here("output", "not-for-review", "AER_results"))
+# 
+# hr_dir <- "output/review/model"
+# table_2_dir <- "output/review/descriptives"
+# aer_raw_results_dir <- "output/not-for-review/AER_results"
+# aer_results_dir <- "output/review/AER_results"
 
 #-------------------------Call AER function-------------------------------------
 source(file.path(scripts_dir,"Absolute_excess_risk_function.R"))
@@ -28,73 +36,91 @@ library(purrr)
 library(data.table)
 library(tidyverse)
 
-agelabels <- c("18_39", "40_59", "60_79", "80_110")
-
 #-------------------------------
 #Step 1. AER for active analyses
 #-------------------------------
 #1. Define the active analyses
-active <- readr::read_rds("lib/active_analyses.rds")                             # selects active analyses
+active <- readr::read_rds("lib/active_analyses.rds")                             
 active <- active[active$active==TRUE,]   
 
 #Preprocess the active analyses
-active$event <- gsub("out_date_","",active$outcome_variable)                                     # refine event name                                                    
-active[,c("active","outcome","outcome_variable","prior_history_var","covariates")] <- NULL       # removes un used columns
+active$event <- gsub("out_date_","",active$outcome_variable)                                                                                        
+active[,c("active","outcome","outcome_variable","prior_history_var","covariates","model","cohort")] <- NULL       
+
+#Converts to long-format
 active <- tidyr::pivot_longer(active, 
-                              cols = setdiff(colnames(active),c("event","model","cohort")), 
-                              names_to = "strata")                                               # converts to long data                                        
-active <- active[active$value==TRUE, c("event","model","cohort","strata")]                       # refines to active models                         
-active$model <- ifelse(active$model=="all","mdl_agesex;mdl_max_adj",active$model)                # includes 2 model types     
-active <- tidyr::separate_rows(active, model, sep = ";")                                         # separate rows for each model
-active$cohort <- ifelse(active$cohort=="all","vaccinated;electively_unvaccinated",active$cohort) # includes 2 cohorts
-active <- tidyr::separate_rows(active, cohort, sep = ";")                                        # separate rows for each cohort
+                              cols = setdiff(colnames(active),c("event")), 
+                              names_to = "strata")                                                                          
+active <- active[active$value==TRUE, c("event","strata")]                               
+
+active$model <- "mdl_max_adj"                 
+
+#Add cohorts
+active <- crossing(active, c("pre_vaccination","vaccinated","electively_unvaccinated"))
+colnames(active) <- c("event", "subgroup", "model", "cohort")
 
 #Focus only on aer analyses
-active <- subset(active, startsWith(active$strata,"aer_"))                      # keep only aer subgroups
-active <- subset(active, active$model=="mdl_max_adj")                           # only require aer for full model
+active <- active %>% filter(startsWith(subgroup,"aer_") & model=="mdl_max_adj")
 
-colnames(active) <- c("event","model","cohort","subgroup")
-active <- active %>% select(-model, everything())                                                  #Order the columns 
+#Add HR timepoint terms 
+active_reduced <- crossing(active, c("days0_28","days28_197","days197_535"))
+colnames(active_reduced) <- c("event", "subgroup", "model", "cohort","term")
+active_reduced$time_points <- "reduced"
 
+active_normal <- crossing(active, c("days0_7","days7_14","days14_28","days28_56","days56_84","days84_197","days197_535"))  
+colnames(active_normal) <- c("event", "subgroup", "model", "cohort","term")
+active_normal$time_points <- "normal"
+results <- rbind(active_reduced,active_normal)
 
+rm(active_reduced,active_normal)
 #----------------------
 #Step 2. Load results
 #----------------------
 #-----------------------------Input hazard ratios-------------------------------
-hr_files=list.files(path = hr_dir, pattern = "suppressed_compiled_HR_results_*")
-hr_files=hr_files[endsWith(hr_files,"_to_release.csv")]
-hr_files=paste0(hr_dir,"/",hr_files)
-hr_files
-input <- purrr::pmap(list(hr_files),
+hr_files=list.files(path = results_dir, pattern = "suppressed_compiled_HR_results_*")
+hr_files=hr_files[endsWith(hr_files,".csv")]
+hr_files=paste0(results_dir,"/", hr_files)
+hr_file_paths <- pmap(list(hr_files),
                       function(fpath){
                         df <- fread(fpath)
-                        return(df)})
-input=rbindlist(input, fill=TRUE)
+                        return(df)
+                      })
+input <- rbindlist(hr_file_paths, fill=TRUE)
 
                                                             
 #-------------------Select required columns and term----------------------------
 input <- input %>% 
-  select(-conf.low, -conf.high, -std.error,-robust.se, -P, -redacted_results) %>%
-  filter(str_detect(term, "^days"))
+  filter(str_detect(term, "^days")
+         & (startsWith(subgroup, "aer_") | subgroup == "main")
+         & model=="mdl_max_adj"
+         & results_fitted == "fitted_successfully"
+         & estimate != "[Redacted]") %>%
+  select(event,cohort,subgroup,model,time_points,term,estimate)
+  
 
 #Focus for aer analyses
-input <- subset(input, input$subgroup=="main")                                  # only aer in main analyses
-input <- subset(input, input$model=="mdl_max_adj")                              # only require aer for full model
-input <- input %>% select(-c("subgroup","total_covid19_cases"))                 # duplicate variables also available in Table 2 (more relevent)
+#input <- input %>% filter(startsWith(subgroup, "aer_"))                             
+#input <- subset(input, input$model=="mdl_max_adj")                              # only require aer for full model
+#input <- input %>% select(-c("subgroup","total_covid19_cases"))                 # duplicate variables also available in Table 2 (more relevent)
 
 #---------------------------------Input Table 2---------------------------------
-table_2_vaccinated <- read_csv(paste0(table_2_dir,"/table2_vaccinated.csv"))
-table_2_electively_unvaccinated <- read_csv(paste0(table_2_dir,"/table2_electively_unvaccinated.csv"))
-table_2 <- rbind(table_2_vaccinated,table_2_electively_unvaccinated)
+table2_pre_vax <- read.csv(paste0(results_dir,"table2_pre_vaccination_cvd.csv"))
+table2_vax <- read.csv(paste0(results_dir,"table2_vaccinated.csv"))
+table2_unvax <- read.csv(paste0(results_dir,"table2_electively_unvaccinated.csv"))
+
+table2_pre_vax <- rename(table2_pre_vax, cohort = cohort_name)
+table2_vax <- rename(table2_vax, cohort = cohort_to_run)
+table2_unvax <- rename(table2_unvax, cohort = cohort_to_run)
+
+table_2 <- rbind(table2_pre_vax, table2_vax,table2_unvax)
+rm(table2_pre_vax,table2_vax,table2_unvax)
 
 #-------------------Select required columns and term----------------------------
-table_2 <- table_2 %>% select(subgroup, event, cohort_to_run,unexposed_person_days,unexposed_event_count,total_covid19_cases)
-table_2$event <- gsub("out_date_","",table_2$event)
-colnames(table_2)<- c("subgroup","event","cohort","unexposed_person_days","unexposed_event_count","total_covid19_cases")
-rm(table_2_vaccinated,table_2_electively_unvaccinated)
-
 #Focus for aer analyses
-table_2 <- subset(table_2, startsWith(table_2$subgroup,"aer_"))                                       # keep only aer values
+table_2 <- table_2 %>% select(subgroup, event, cohort,unexposed_person_days,unexposed_event_count,total_covid19_cases) %>%
+                      filter(startsWith(subgroup, "aer_"))
+            
+table_2$event <- gsub("out_date_","",table_2$event)
 
 # # Non-hospitalised/hospitalised unexposed person days are the same as in the
 # # main analysis so copy these values and add onto table
@@ -111,16 +137,47 @@ table_2 <- subset(table_2, startsWith(table_2$subgroup,"aer_"))                 
 #   }
 # }
 
-input <- input %>% left_join(table_2, by=c("event","cohort"))
-input <- input %>% filter(!is.na(unexposed_person_days) & unexposed_event_count != "[Redacted]")
+
+tmp <- input %>% filter(subgroup == "main")
+tmp$subgroup <- NULL
+results <- results %>% left_join(tmp, by=c("event","cohort","model","time_points","term"))
+results <- results %>% rename(estimate_main = estimate)
+
+tmp <- input %>% filter(subgroup != "main")
+results <- results %>% left_join(tmp, by=c("event","cohort","model","time_points","term","subgroup"))
+results <- results %>% rename(estimate_subgroup = estimate)
+
+#Remove any rows where both HR estimates are NA
+results <- results %>% filter(!is.na(estimate_main) | !is.na(estimate_subgroup))
+
+results <- results %>% left_join(table_2, by=c("event","cohort","subgroup"))
+
+
+results <- results %>% mutate(across(c(estimate_main,estimate_subgroup, unexposed_person_days, unexposed_event_count, total_covid19_cases), as.numeric))
+#input <- input %>% filter(!is.na(unexposed_person_days) & unexposed_event_count != "[Redacted]")
 
 #Determine which analyses have a complete set of results so that AER can be calculated
-df <- input %>% select(event, subgroup, model, cohort) %>% distinct
+df <- results %>% select(event, subgroup, model, cohort) %>% distinct
 active_available <- merge(active,df)
-results_unavailable <- active %>% anti_join(active_available)
-rm(active,table_2,df)
-# rm(tmp)
+active_unavailable <- active %>% anti_join(active_available)
+rm(active,table_2,df,input,tmp)
 
+input <- results
+rm(results)
+
+input <- input %>% filter(time_points == "reduced")
+#Need to update once we've decided if we're using age/sex subgroups HRs or overall HRs
+# Some events have results for both normal and reduced time points
+# Where possible use normal time points, otherwise used reduced
+
+# input <- input %>%
+#   group_by(event,subgroup,cohort,time_points) %>%
+#   dplyr::mutate(time_points_to_use = case_when(
+#     any(time_points == "normal") ~ "normal",
+#     TRUE ~ "reduced"))
+# 
+# input <- input %>% filter(time_points == time_points_to_use) %>%
+#                   select(-time_points_to_use)
 #-------------------------
 #Step 3. Run AER function
 #-------------------------
@@ -131,6 +188,7 @@ lapply(split(active_available,seq(nrow(active_available))),
            event_of_interest = active_available$event,
            cohort_of_interest = active_available$cohort,
            model_of_interest = active_available$model,
+           subgroup_of_interest = active_available$subgroup,
            input))
 
 
@@ -138,29 +196,29 @@ lapply(split(active_available,seq(nrow(active_available))),
 #Step 4. Compile results
 #----------------------
 #------------------------------Compile the results------------------------------
-AER_files=list.files(path = aer_raw_results_dir, pattern = "AER_raw_results_*")
-AER_files=paste0(aer_raw_results_dir,"/",AER_files)
+AER_files=list.files(path = aer_raw_output_dir, pattern = "lifetable_")
+AER_files=paste0(aer_raw_output_dir,"/",AER_files)
 AER_compiled_results <- purrr::pmap(list(AER_files),
                                     function(fpath){
                                       df <- fread(fpath)
                                       return(df)})
 AER_compiled_results=rbindlist(AER_compiled_results, fill=TRUE)
-write.csv(AER_compiled_results, paste0(aer_results_dir,"/AER_compiled_results.csv"), row.names = F)
+write.csv(AER_compiled_results, paste0(aer_compiled_output_dir,"/AER_compiled_results.csv"), row.names = F)
 
 
 #--------------------------Compile results for AER figure-----------------------
 
-lt_files=list.files(path = aer_raw_results_dir, pattern = "lifetable_*")
-lt_files=paste0(aer_raw_results_dir,"/",lt_files)
-compiled_lifetables <- purrr::pmap(list(lt_files),
-                                   function(fpath){
-                                     df <- fread(fpath)
-                                     return(df)
-                                   })
-compiled_lifetables=rbindlist(compiled_lifetables, fill=TRUE)
-
-#3.output the csv
-write.csv(compiled_lifetables, paste0(aer_results_dir,"/Figure4_compiled_lifetables.csv"), row.names = F)
+# lt_files=list.files(path = aer_raw_results_dir, pattern = "lifetable_*")
+# lt_files=paste0(aer_raw_results_dir,"/",lt_files)
+# compiled_lifetables <- purrr::pmap(list(lt_files),
+#                                    function(fpath){
+#                                      df <- fread(fpath)
+#                                      return(df)
+#                                    })
+# compiled_lifetables=rbindlist(compiled_lifetables, fill=TRUE)
+# 
+# #3.output the csv
+# write.csv(compiled_lifetables, paste0(aer_results_dir,"/Figure4_compiled_lifetables.csv"), row.names = F)
 
 
 
