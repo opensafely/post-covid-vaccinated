a6b87c193bef4bafc13551b425ad207c09663215
RochelleKnight <rochelle.knight@bristol.ac.uk>
2022-09-22 17:17:10 +0100

    Update Figure_4.R

--- analysis/figures/Figure_4.R
+++ analysis/figures/Figure_4.R
@@ -1,10 +1,4 @@
-#Project: Vaccinated delta wave population study
-#Scripts: Renin Toms, Venexia Walker
-#Reviewer: Genevieve Cezard
-
-#TO RUN OUTSIDE OPENSAFELY
-# 1. load the right input data and make sure of the file names and variable structure
-# 2. Cntrl+A run the whole script and find the .png graph files in working directory
+# Plots Figure 4 - AER
 
 library(purrr)
 library(data.table)
@@ -14,78 +8,90 @@ library(dplyr)
 library(ggplot2)
 library(RColorBrewer)
 library(gridExtra)
+library(plyr)
 
-aer_results_dir <- "output/review/AER_results"
+aer_output_dir <- "C:/Users/zy21123/OneDrive - University of Bristol/Documents/OpenSAFELY/Outputs/Figures/AER/compiled_results/"
 
-event_of_interest <- c("ate","vte")
-model_of_interest <- c("mdl_max_adj")
-cohort_name <- c("vaccinated","electively_unvaccinated")
+event_of_interest <- c("ate","vte","ate_primary_position","vte_primary_position")
+cohort_name <- c("pre_vaccination", "vaccinated","electively_unvaccinated")
 
 active_analyses <- read_rds("lib/active_analyses.rds")
 active_analyses$outcome_variable <- gsub("out_date_","",active_analyses$outcome_variable)
 
 #Load data
-lifetables <- readr::read_csv(paste0(aer_results_dir,"/Figure4_compiled_lifetables.csv"))
+lifetables <- readr::read_csv(paste0(aer_output_dir,"/AER_compiled_results.csv"))
 
-#------------------------Filter to outcomes of interest-------------------------
-lifetables <- lifetables %>% filter(event %in% event_of_interest & model %in% model_of_interest)
-
-#----------------------Filter to subgroups of interest--------------------------
-lifetables <- lifetables %>% filter(str_detect(subgroup, c("^main","^sex","^age")))
-
+#???? Do we need to do this ?????
+lifetables$excess_risk_main <- lifetables$excess_risk_main *100
+lifetables$excess_risk_subgroup <- lifetables$excess_risk_subgroup *100
 #-------------------------Make event names 'nice' ------------------------------
 lifetables <- lifetables %>% left_join(active_analyses %>% select(outcome, outcome_variable), by = c("event"="outcome_variable"))
 
-#-------------------------------Format subgroups names--------------------------
-lifetables$subgroup <- ifelse(lifetables$subgroup=="main","Combined",lifetables$subgroup)
-lifetables$subgroup <- ifelse(lifetables$subgroup=="agegp_18_39","Age group: 18-39",lifetables$subgroup)
-lifetables$subgroup <- ifelse(lifetables$subgroup=="agegp_40_59","Age group: 40-59",lifetables$subgroup)
-lifetables$subgroup <- ifelse(lifetables$subgroup=="agegp_60_79","Age group: 60-79",lifetables$subgroup)
-lifetables$subgroup <- ifelse(lifetables$subgroup=="agegp_80_110","Age group: 80-110",lifetables$subgroup)
-lifetables$subgroup <- ifelse(lifetables$subgroup=="sex_Male","Sex: Male",lifetables$subgroup)
-lifetables$subgroup <- ifelse(lifetables$subgroup=="sex_Female","Sex: Female",lifetables$subgroup)
+#-------------------------------Format group names--------------------------
+lifetables$agegroup <- NA
+lifetables$agegroup <- ifelse(grepl("18_39",lifetables$subgroup),"Age group: 18-39",lifetables$agegroup)
+lifetables$agegroup <- ifelse(grepl("40_59",lifetables$subgroup),"Age group: 40-59",lifetables$agegroup)
+lifetables$agegroup <- ifelse(grepl("60_79",lifetables$subgroup),"Age group: 60-79",lifetables$agegroup)
+lifetables$agegroup <- ifelse(grepl("80_110",lifetables$subgroup),"Age group: 80-110",lifetables$agegroup)
+
+lifetables$sex <- NA
+lifetables$sex <- ifelse(grepl("Female",lifetables$subgroup),"Sex: Female",lifetables$sex)
+lifetables$sex <- ifelse(grepl("Male",lifetables$subgroup),"Sex: Male",lifetables$sex)
 
 # Specify line colours ---------------------------------------------------------
 
-lifetables$colour <- ""
-lifetables$colour <- ifelse(lifetables$subgroup=="Combined","#000000",lifetables$colour)
-lifetables$colour <- ifelse(lifetables$subgroup=="Age group: 18-39","#006d2c",lifetables$colour)
-lifetables$colour <- ifelse(lifetables$subgroup=="Age group: 40-59","#31a354",lifetables$colour)
-lifetables$colour <- ifelse(lifetables$subgroup=="Age group: 60-79","#74c476",lifetables$colour)
-lifetables$colour <- ifelse(lifetables$subgroup=="Age group: 80-110","#bae4b3",lifetables$colour)
-lifetables$colour <- ifelse(lifetables$subgroup=="Sex: Male","#cab2d6",lifetables$colour)
-lifetables$colour <- ifelse(lifetables$subgroup=="Sex: Female","#6a3d9a",lifetables$colour)
+lifetables$colour <- NA
+lifetables$colour <- ifelse(lifetables$agegroup=="Age group: 18-39","#006d2c",lifetables$colour)
+lifetables$colour <- ifelse(lifetables$agegroup=="Age group: 40-59","#31a354",lifetables$colour)
+lifetables$colour <- ifelse(lifetables$agegroup=="Age group: 60-79","#74c476",lifetables$colour)
+lifetables$colour <- ifelse(lifetables$agegroup=="Age group: 80-110","#bae4b3",lifetables$colour)
 
 # Specify line types ---------------------------------------------------------
+lifetables$linetype <- NA
+lifetables$linetype <- ifelse(lifetables$sex=="Sex: Male","solid",lifetables$linetype)
+lifetables$linetype <- ifelse(lifetables$sex=="Sex: Female","dotted",lifetables$linetype)
 
-lifetables$line <- ""
-lifetables$line <- ifelse(lifetables$subgroup=="Combined","solid",lifetables$line)
-lifetables$line <- ifelse(lifetables$subgroup=="Age group: 18-39","dotted",lifetables$line)
-lifetables$line <- ifelse(lifetables$subgroup=="Age group: 40-59","dotted",lifetables$line)
-lifetables$line <- ifelse(lifetables$subgroup=="Age group: 60-79","dotted",lifetables$line)
-lifetables$line <- ifelse(lifetables$subgroup=="Age group: 80-110","dotted",lifetables$line)
-lifetables$line <- ifelse(lifetables$subgroup=="Sex: Male","longdash",lifetables$line)
-lifetables$line <- ifelse(lifetables$subgroup=="Sex: Female","longdash",lifetables$line)
+#Filter the lifatbles to non-NA results split by using the HRs from the overall results (hr_main)
+# & using the age/sex HRs (hr_subgroup)
+lifetables_main <- lifetables %>% filter(!is.na(hr_main))
+lifetables_subgroup <- lifetables %>% filter(!is.na(hr_subgroup))
 
 #--------------Option 1: Indivdual plots for each outcome and cohort------------
 
 for(cohort_of_interest in cohort_name){
-  for(outcome_name in event_of_interest){
-    df=lifetables %>% filter(cohort == cohort_of_interest & event==outcome_name )
+  for(outcome_position in c("any_position","primary_position")){
     
-    sub_group_levels <-c()
-    for(i in c("Combined","Age group: 18-39","Age group: 40-59","Age group: 60-79","Age group: 80-110",
-               "Sex: Female","Sex: Male")){
-      levels_available <- unique(df$subgroup)
+    if(outcome_position == "any_position"){
+      df=lifetables_main %>% filter(cohort == cohort_of_interest & !str_detect(event, "primary_position") )
+    }else{
+      df=lifetables_main %>% filter(cohort == cohort_of_interest & str_detect(event, "primary_position") )
+    }
+    
+    #Set agegroup levels as factor
+    agegroup_levels <-c()
+    for(i in c("Age group: 18-39","Age group: 40-59","Age group: 60-79","Age group: 80-110")){
+      levels_available <- unique(df$agegroup)
       if(i %in% levels_available){
-        sub_group_levels <- append(sub_group_levels,i)
+        agegroup_levels <- append(agegroup_levels,i)
       }
     }
     
-    df$subgroup <- factor(df$subgroup, levels=sub_group_levels)
+    df$agegroup <- factor(df$agegroup, levels=agegroup_levels)
     
+    #Set sex levels as factor
+    sex_levels <-c()
+    for(i in c("Sex: Male","Sex: Female")){
+      levels_available <- unique(df$sex)
+      if(i %in% levels_available){
+        sex_levels <- append(sex_levels,i)
+      }
+    }
+    
+    df$sex <- factor(df$sex, levels=sex_levels)
+    
+    #Set colour levels as factor
     colour_levels <-c()
-    for(i in c("#000000","#006d2c","#31a354","#74c476","#bae4b3","#6a3d9a","#cab2d6")){
+    for(i in c("#006d2c","#31a354","#74c476","#bae4b3")){
       levels_available <- unique(df$colour)
       if(i %in% levels_available){
         colour_levels <- append(colour_levels,i)
@@ -93,21 +99,32 @@ for(cohort_of_interest in cohort_name){
     } 
     df$colour <- factor(df$colour, levels=colour_levels)
     
+    #Set linetype levels as factor
+    linetype_levels <-c()
+    for(i in c("solid","dotted")){
+      levels_available <- unique(df$linetype)
+      if(i %in% levels_available){
+        linetype_levels <- append(linetype_levels,i)
+      }
+    } 
+    df$linetype <- factor(df$linetype, levels=linetype_levels)
+    
+    
     #Test to see error bars as in dummy data the CI is too small so can't see it
     #df$CIp.low<-df$AERp - 0.02
     #df$CIp.high<-df$AERp + 0.02
     
-    plot<-ggplot2::ggplot(data = df, 
-                    mapping = ggplot2::aes(x = days, y = AERp, color = subgroup, shape = subgroup, fill = subgroup)) +
+    ggplot2::ggplot(data = df, 
+                    mapping = ggplot2::aes(x = days, y = excess_risk_main, color = agegroup, shape = agegroup, fill = agegroup, linetype = sex)) +
       #ggplot2::geom_hline(colour = "#A9A9A9") +
-      geom_ribbon(aes(ymin = CIp.low, ymax = CIp.high), alpha = 0.1)+
+      #geom_ribbon(aes(ymin = CIp.low, ymax = CIp.high), alpha = 0.1)+
       ggplot2::geom_line() +
-      ggplot2::scale_x_continuous(breaks = c(0,20,40,60,80,100,120,140,160,180,200),limits = c(0,200))+
-      ggplot2::scale_fill_manual(values = levels(df$colour), labels = levels(df$subgroup)) +
-      ggplot2::scale_color_manual(values = levels(df$colour), labels = levels(df$subgroup)) +
-      ggplot2::labs(x = "Days since COVID-19 diagnosis", y = "Cumulative difference in absolute risk  (%)", title =df$outcome[1] ) +
-      ggplot2::ggtitle(df$outcome[1])+
-      ggplot2::guides(fill=ggplot2::guide_legend(ncol = length(sub_group_levels), byrow = TRUE)) +
+      ggplot2::scale_x_continuous(lim = c(0,round_any(max(df$days, na.rm = T),20, f= ceiling)), breaks = seq(0,round_any(max(df$days, na.rm = T),20, f= ceiling),20))+ 
+      ggplot2::scale_fill_manual(values = levels(df$colour), labels = levels(df$agegroup)) +
+      ggplot2::scale_color_manual(values = levels(df$colour), labels = levels(df$agegroup)) +
+      ggplot2::scale_linetype_manual(values = levels(df$linetype), labels = levels(df$sex))+
+      ggplot2::labs(x = "Days since COVID-19 diagnosis", y = "Cumulative difference in absolute risk  (%)") +
+      ggplot2::guides(fill=ggplot2::guide_legend(ncol = 6, byrow = TRUE)) +
       ggplot2::theme_minimal() +
       ggplot2::theme(panel.grid.major.x = ggplot2::element_blank(),
                      panel.grid.minor = ggplot2::element_blank(),
@@ -117,80 +134,95 @@ for(cohort_of_interest in cohort_name){
                      legend.title = ggplot2::element_blank(),
                      legend.position="bottom",
                      plot.background = ggplot2::element_rect(fill = "white", colour = "white"),
-                     plot.title = element_text(hjust = 0.5)) 
-
-   
- 
-  #assign(paste0(outcome_name,"_",cohort_of_interest),plot)
-  ggsave(paste0(aer_results_dir, "/figure_4_",outcome_name,"_",cohort_of_interest,".png"), height = 210, width = 297, unit = "mm", dpi = 600, scale = 1)
+                     plot.title = element_text(hjust = 0.5))+
+      ggplot2::facet_wrap(outcome~., ncol = 2)
+    
+    ggsave(paste0(aer_output_dir, "/figure_4_",cohort_of_interest,"_",outcome_position, "_using_overall_HRs.png"), height = 210, width = 297, unit = "mm", dpi = 600, scale = 1)
   }
 }
 
-#grid.arrange(ate_vaccinated, vte_vaccinated, nrow = 1)
-#grid.arrange(ate_electively_unvaccinated, vte_electively_unvaccinated, nrow = 1)
-
-
-#-----------Option 2: Plots for each cohort containing both outcomes------------
 
 for(cohort_of_interest in cohort_name){
-  df=lifetables %>% filter(cohort == cohort_of_interest)
-  
-  sub_group_levels <-c()
-  for(i in c("Combined","Age group: 18-39","Age group: 40-59","Age group: 60-79","Age group: 80-110",
-             "Sex: Female","Sex: Male")){
-    levels_available <- unique(df$subgroup)
-    if(i %in% levels_available){
-      sub_group_levels <- append(sub_group_levels,i)
+  for(outcome_position in c("any_position","primary_position")){
+    
+    if(outcome_position == "any_position"){
+      df=lifetables_subgroup %>% filter(cohort == cohort_of_interest & !str_detect(event, "primary_position") )
+    }else{
+      df=lifetables_subgroup %>% filter(cohort == cohort_of_interest & str_detect(event, "primary_position") )
     }
+    
+    #Set agegroup levels as factor
+    agegroup_levels <-c()
+    for(i in c("Age group: 18-39","Age group: 40-59","Age group: 60-79","Age group: 80-110")){
+      levels_available <- unique(df$agegroup)
+      if(i %in% levels_available){
+        agegroup_levels <- append(agegroup_levels,i)
+      }
+    }
+    
+    df$agegroup <- factor(df$agegroup, levels=agegroup_levels)
+    
+    #Set sex levels as factor
+    sex_levels <-c()
+    for(i in c("Sex: Male","Sex: Female")){
+      levels_available <- unique(df$sex)
+      if(i %in% levels_available){
+        sex_levels <- append(sex_levels,i)
+      }
+    }
+    
+    df$sex <- factor(df$sex, levels=sex_levels)
+    
+    #Set colour levels as factor
+    colour_levels <-c()
+    for(i in c("#006d2c","#31a354","#74c476","#bae4b3")){
+      levels_available <- unique(df$colour)
+      if(i %in% levels_available){
+        colour_levels <- append(colour_levels,i)
+      }
+    } 
+    df$colour <- factor(df$colour, levels=colour_levels)
+    
+    #Set linetype levels as factor
+    linetype_levels <-c()
+    for(i in c("solid","dotted")){
+      levels_available <- unique(df$linetype)
+      if(i %in% levels_available){
+        linetype_levels <- append(linetype_levels,i)
+      }
+    } 
+    df$linetype <- factor(df$linetype, levels=linetype_levels)
+    
+    
+    #Test to see error bars as in dummy data the CI is too small so can't see it
+    #df$CIp.low<-df$AERp - 0.02
+    #df$CIp.high<-df$AERp + 0.02
+    
+    ggplot2::ggplot(data = df, 
+                    mapping = ggplot2::aes(x = days, y = excess_risk_main, color = agegroup, shape = agegroup, fill = agegroup, linetype = sex)) +
+      #ggplot2::geom_hline(colour = "#A9A9A9") +
+      #geom_ribbon(aes(ymin = CIp.low, ymax = CIp.high), alpha = 0.1)+
+      ggplot2::geom_line() +
+      ggplot2::scale_x_continuous(lim = c(0,round_any(max(df$days, na.rm = T),20, f= ceiling)), breaks = seq(0,round_any(max(df$days, na.rm = T),20, f= ceiling),20)) +
+      ggplot2::scale_fill_manual(values = levels(df$colour), labels = levels(df$agegroup)) +
+      ggplot2::scale_color_manual(values = levels(df$colour), labels = levels(df$agegroup)) +
+      ggplot2::scale_linetype_manual(values = levels(df$linetype), labels = levels(df$sex))+
+      ggplot2::labs(x = "Days since COVID-19 diagnosis", y = "Cumulative difference in absolute risk  (%)") +
+      ggplot2::guides(fill=ggplot2::guide_legend(ncol = 6, byrow = TRUE)) +
+      ggplot2::theme_minimal() +
+      ggplot2::theme(panel.grid.major.x = ggplot2::element_blank(),
+                     panel.grid.minor = ggplot2::element_blank(),
+                     panel.spacing.x = ggplot2::unit(0.5, "lines"),
+                     panel.spacing.y = ggplot2::unit(0, "lines"),
+                     legend.key = ggplot2::element_rect(colour = NA, fill = NA),
+                     legend.title = ggplot2::element_blank(),
+                     legend.position="bottom",
+                     plot.background = ggplot2::element_rect(fill = "white", colour = "white"),
+                     plot.title = element_text(hjust = 0.5))+
+      ggplot2::facet_wrap(outcome~., ncol = 2)
+    
+    ggsave(paste0(aer_output_dir, "/figure_4_",cohort_of_interest,"_",outcome_position, "_using_age_sex_subgroup_HRs.png"), height = 210, width = 297, unit = "mm", dpi = 600, scale = 1)
   }
-  
-  df$subgroup <- factor(df$subgroup, levels=sub_group_levels)
-  
-  colour_levels <-c()
-  for(i in c("#000000","#006d2c","#31a354","#74c476","#bae4b3","#6a3d9a","#cab2d6")){
-    levels_available <- unique(df$colour)
-    if(i %in% levels_available){
-      colour_levels <- append(colour_levels,i)
-    }
-  } 
-  df$colour <- factor(df$colour, levels=colour_levels)
-  
-  df$line <- factor(df$line, levels = c("solid","dotted","longdash"))
-  
-  #Test to see error bars as in dummy data the CI is too small so can't see it
-  #df$CIp.low<-df$AERp - 0.02
-  #df$CIp.high<-df$AERp + 0.02
-  
-  ggplot2::ggplot(data = df, 
-                        mapping = ggplot2::aes(x = days, y = AERp, color = subgroup, shape = subgroup, fill = subgroup, linetype=subgroup)) +
-    #ggplot2::geom_hline(colour = "#A9A9A9") +
-    geom_ribbon(aes(ymin = CIp.low, ymax = CIp.high), alpha = 0.1)+
-    ggplot2::geom_line() +
-    ggplot2::scale_x_continuous(breaks = c(0,20,40,60,80,100,120,140,160,180,200),limits = c(0,200))+
-    ggplot2::scale_fill_manual(values = levels(df$colour), labels = levels(df$subgroup)) +
-    ggplot2::scale_color_manual(values = levels(df$colour), labels = levels(df$subgroup)) +
-    #Might need to have a think about line types as can't get it to work like colour
-    #ggplot2::scale_linetype_manual(values = c("solid","dotted","dotdash","dashed",rep("solid",2)), labels = levels(df$subgroup)) +
-    ggplot2::labs(x = "Days since COVID-19 diagnosis", y = "Cumulative difference in absolute risk  (%)") +
-    ggplot2::guides(fill=ggplot2::guide_legend(ncol = length(sub_group_levels), byrow = TRUE)) +
-    ggplot2::theme_minimal() +
-    ggplot2::theme(panel.grid.major.x = ggplot2::element_blank(),
-                   panel.grid.minor = ggplot2::element_blank(),
-                   panel.spacing.x = ggplot2::unit(0.5, "lines"),
-                   panel.spacing.y = ggplot2::unit(0, "lines"),
-                   legend.key = ggplot2::element_rect(colour = NA, fill = NA),
-                   legend.title = ggplot2::element_blank(),
-                   legend.position="bottom",
-                   plot.background = ggplot2::element_rect(fill = "white", colour = "white"))+
-    ggplot2::facet_wrap(outcome~.,ncol=2)
-  
-  ggsave(paste0(aer_results_dir, "/figure_4_",cohort_of_interest,".png"), height = 210, width = 297, unit = "mm", dpi = 600, scale = 1)
 }
 
-
-
-
-
-
-  
 
