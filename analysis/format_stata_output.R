393da72d5854dc6502c503c19e4f41ceec2d3ce3
Venexia Walker <venexia.walker@bristol.ac.uk>
2022-10-12 15:58:04 +0100

    Remove unused rows from output file

--- analysis/format_stata_output.R
+++ analysis/format_stata_output.R
@@ -90,6 +90,8 @@ df <- tidyr::pivot_wider(df,
 
 # Make names match R output ----------------------------------------------------
 
+df <- df[df$model=="max" | (df$model=="min" & df$term %in% c("days0_28","days28_197","1.sex","2.sex","age_spline1","age_spline2")),]
+
 df <- df[order(df$source, df$model),
          c("source","term","model","b","se","lci","uci","subjects")]
 
