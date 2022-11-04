668a266f451bd9d639ad07e5d4bd3e9f653063bd
RochelleKnight <rochelle.knight@bristol.ac.uk>
2022-11-04 14:25:04 +0000

    Update format_supp_table_3_AER.R

--- analysis/figures/format_supp_table_3_AER.R
+++ analysis/figures/format_supp_table_3_AER.R
@@ -1,40 +1,88 @@
+library(dplyr)
+library(readr)
+library(stringr)
+library(tidyr)
+
 aer_output_dir <- "C:/Users/zy21123/OneDrive - University of Bristol/Documents/OpenSAFELY/Outputs/Figures/AER/compiled_results/"
+results_dir <- "C:/Users/zy21123/OneDrive - University of Bristol/Documents/OpenSAFELY/Outputs/release/"
 
-lifetables <- readr::read_csv(paste0(aer_output_dir,"/AER_compiled_results.csv"))
+#Read in AER
+df <- readr::read_csv(paste0(aer_output_dir,"/AER_compiled_results.csv"))
 
-lifetables <- lifetables %>% 
+#Select max day of follow up (196 for vax/unvax, 534 for pre-vax)
+df <- df %>% 
   group_by(event, subgroup, time_points, cohort) %>%
   filter(days == max(days) & subgroup != "aer_overall" & !is.na(AER_main) ) %>%
   select(event, subgroup, cohort, subgroup, AER_main, time_points, days)%>% ungroup()
 
-lifetables <- lifetables %>% 
+#Sum over all subgroups to get total
+df <- df %>% 
   group_by(event, time_points, cohort) %>%
  summarise(total = sum(AER_main))
 
-lifetables <- tidyr::pivot_wider(lifetables, names_from = "cohort", values_from = "total")
+#Add person years of follow up
+table2_pre_vax <- read.csv(paste0(results_dir,"table2_pre_vaccination_cvd.csv"))
+table2_vax <- read.csv(paste0(results_dir,"table2_vaccinated.csv"))
+table2_unvax <- read.csv(paste0(results_dir,"table2_electively_unvaccinated.csv"))
+table2_pre_vax <- table2_pre_vax %>% rename(cohort_to_run = cohort_name)
 
-# Add tidy names
+table2 <- rbind(table2_pre_vax,table2_vax,table2_unvax)
+rm(table2_pre_vax,table2_vax,table2_unvax)
+
+#To get total follow up for main analysis need to sum unexposed person days of follow up & exposed person days
+table2 <- table2 %>% select(event, subgroup, cohort_to_run, total_person_days, unexposed_person_days) %>% 
+  filter(subgroup %in% c("covid_pheno_hospitalised","covid_pheno_non_hospitalised"))
+
+table2$exposed_person_days <- table2$total_person_days - table2$unexposed_person_days
+table2$total_person_days <- NULL
+table2 <- pivot_longer(table2, cols = c("unexposed_person_days","exposed_person_days"), names_to = "time_periods", values_to = "follow_up")
+
+#Unexposed person days is the same for both hospitalised and non-hospitalised analysis
+table2 <- table2 %>% filter(subgroup == "covid_pheno_hospitalised"
+                            | (subgroup == "covid_pheno_non_hospitalised" & time_periods == "exposed_person_days"))
+
+#Sum follow up
+table2 <- table2 %>% group_by(event, cohort_to_run) %>%
+  summarise(total_follow_up = sum(follow_up))
+
+#Convert from days to years
+table2$total_follow_up <- table2$total_follow_up/365.2
+table2$event <- gsub("out_date_","",table2$event)
+
+#Left join follow up onto AER table
+df <- df %>% left_join(table2, by = c("event"="event", "cohort"="cohort_to_run"))
+df$excess_events_per_1000_person_years <- df$total * (1000/df$total_follow_up)
+
+df <- pivot_longer(df, cols = c(total, total_follow_up, excess_events_per_1000_person_years), names_to = "summary", values_to = "total")
+df <- pivot_wider(df, names_from = cohort, values_from = total)
+
+df <- df %>% rename("Pre-vaccination cohort"="pre_vaccination",
+                    "Vaccinated cohort"="vaccinated",
+                    "Unvaccinated cohort"="electively_unvaccinated")
+
+df$summary <- ifelse(df$summary == "total", "Total excess events",df$summary)
+df$summary <- ifelse(df$summary == "total_follow_up", "Total follow up (years)",df$summary)
+df$summary <- ifelse(df$summary == "excess_events_per_1000_person_years", "Excess events per 1000 person years",df$summary)
+
+#Get tidy names
 active_analyses <- read_rds("lib/active_analyses.rds") %>% filter(active == "TRUE")
 
 outcome_name_table <- active_analyses %>% 
-  select(outcome, outcome_variable) %>% 
-  mutate(outcome_variable=active_analyses$outcome_variable %>% str_replace("out_date_", ""))
+  mutate(outcome_name=active_analyses$outcome_variable %>% str_replace("out_date_", ""))%>%
+  select(outcome, outcome_name)
 
-lifetables <- lifetables %>% left_join(outcome_name_table, by = c("event"="outcome_variable"))
-lifetables$event <- NULL
-
-lifetables <- lifetables %>% select(outcome,time_points, pre_vaccination, vaccinated, electively_unvaccinated)
-colnames(lifetables) <- c("Outcome", "time_points", "Pre-vaccination cohort", "Vaccinated cohort", "Unvaccinated cohort")
-
+df <- df %>% left_join(outcome_name_table, by = c("event"="outcome_name"))
+df$event <- NULL
+df <- df %>% select(outcome,summary,time_points, `Pre-vaccination cohort`, `Vaccinated cohort`, `Unvaccinated cohort`)
 
 for(i in c("any_position","primary_position")){
   for (j in c("reduced", "normal")){
     
     if(i == "any_position"){
-      tmp <- lifetables %>% filter(!Outcome %in% Outcome[grepl("Primary position events",Outcome)]
+      tmp <- df %>% filter(!outcome %in% outcome[grepl("Primary position events",outcome)]
                                  & time_points == j)
     }else{
-      tmp <- lifetables %>% filter(Outcome %in% Outcome[grepl("Primary position events",Outcome)]
+      tmp <- df %>% filter(outcome %in% outcome[grepl("Primary position events",outcome)]
                                    & time_points == j)
     }
     
