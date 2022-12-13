a5108a1d426f2fa07da3db8bb64e60b5db926f4e
RochelleKnight <rochelle.knight@bristol.ac.uk>
2022-12-13 10:37:51 +0000

    Update format_stata_output.R

--- analysis/format_stata_output.R
+++ analysis/format_stata_output.R
@@ -15,8 +15,7 @@ for (f in files) {
   tmp <- readr::read_tsv(file = paste0("output/",f), skip = 2,
                          col_names = c("term",
                                        "b_min","se_min","t_min","lci_min","uci_min","p_min",
-                                       "b_max","se_max","t_max","lci_max","uci_max","p_max"),
-                         show_col_types = FALSE)
+                                       "b_max","se_max","t_max","lci_max","uci_max","p_max"))
   
   ## Make variables numeric
   
@@ -69,7 +68,7 @@ for (f in files) {
   f <- gsub("_cox_model","_stata_median_fup",f)
   f <- gsub(".txt",".csv",f)
   print(f)
-  fup <- readr::read_csv(file = paste0("output/",f), show_col_types = FALSE)
+  fup <- readr::read_csv(file = paste0("output/",f))
   tmp <- merge(tmp, fup, by = "term", all.x = TRUE)
   
   ## Apend to master dataframe
